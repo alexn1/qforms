23f7f7c5670c58d1daf57195f0e0cf40792b46f4
Alexander Nesterenko <alexn140@gmail.com>
2015-06-16 01:51:08 +0700

    for dublicate column names

--- engine/backend/viewer/DataAdapter/SqlDataAdapter/SqlDataAdapter.js
+++ engine/backend/viewer/DataAdapter/SqlDataAdapter/SqlDataAdapter.js
@@ -23,7 +23,7 @@ SqlDataAdapter.prototype.select = function(params, callback) {
     var query = this.dataSource.getQuery(params);
     this._query(query, params, function(rows) {
         callback(rows);
-    });
+    }, true);
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -72,18 +72,44 @@ SqlDataAdapter.prototype.delete = function(row, callback) {
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
-SqlDataAdapter.prototype._query = function(query, params, callback) {
+SqlDataAdapter.prototype._query = function(query, params, callback, select) {
+    select = select !== undefined ? select : false;
     console.log({query:query,params:params});
     this.dataSource.getPool().getConnection(function(err, cnn) {
         if (err) {
             throw err;
         } else {
-            cnn.query({sql:query,typeCast:helper.typeCast}, params, function(err, result, fields) {
+            cnn.query({sql:query,typeCast:helper.typeCast,nestTables: true}, params, function(err, result, fields) {
                 cnn.release();
                 if (err) {
                     throw err;
                 } else {
-                    callback(result);
+                    if (select) {
+                        // for dublicate column names
+                        var fieldCount = {};
+                        for (var j=0;j<fields.length;j++) {
+                            var f = fields[j];
+                            if (!fieldCount[f.name]) {
+                                fieldCount[f.name] = 0;
+                            }
+                            fieldCount[f.name]++;
+                            f.numb = fieldCount[f.name] - 1;
+                        }
+                        var rows = [];
+                        for (var i=0; i<result.length; i++) {
+                            var r = result[i];
+                            var row = {};
+                            for (var j=0; j<fields.length; j++) {
+                                var f = fields[j];
+                                var column = f.name + (f.numb > 0 ? f.numb : '');
+                                row[column] = r[f.table][f.name];
+                            }
+                            rows.push(row);
+                        }
+                        callback(rows);
+                    } else {
+                        callback(result);
+                    }
                 }
             });
         }
