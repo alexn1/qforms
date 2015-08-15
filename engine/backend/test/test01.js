39ee40feedce3379b589eed2cdb5e7cde977651d
Alexander Nesterenko <alexn140@gmail.com>
2015-08-16 01:58:17 +0700

    added test for deleting in row form

--- engine/backend/test/test01.js
+++ engine/backend/test/test01.js
@@ -7,21 +7,40 @@ var ApplicationController = require('../viewer/ModelController/ApplicationContro
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 describe('test01', function() {
+    var application;
+    var key;
+    before(function(done) {
+        ApplicationController.create('../../apps/demo/application1.json', function(_application) {
+            _application.init(function() {
+                application = _application;
+                done();
+            });
+        });
+    });
     it('insert row with RowForm', function(done) {
-        ApplicationController.create('../../apps/demo/application1.json', function(application) {
-            application.init(function() {
-                var context = application.createContext({
-                    row: {
-                        first_name: 'test a',
-                        last_name : 'test b'
-                    }
-                });
-                application.getPage(context, 'employee', function(page) {
-                    page.forms.employee.dataSources.default.insert(context, function(key) {
-                        console.log(key);
-                        done();
-                    });
-                });
+        var context = application.createContext({
+            row: {
+                first_name: 'test a',
+                last_name : 'test b'
+            }
+        });
+        application.getPage(context, 'employee', function(page) {
+            page.forms.employee.dataSources.default.insert(context, function(_key) {
+                key = _key;
+                console.log('key: ', JSON.stringify(_key));
+                done();
+            });
+        });
+    });
+    it('delete row with RowForm', function(done) {
+        var context = application.createContext({
+            row: {
+                id: JSON.parse(key)[0]
+            }
+        });
+        application.getPage(context, 'employee', function(page) {
+            page.forms.employee.dataSources.default.delete(context, function() {
+                done();
             });
         });
     });
