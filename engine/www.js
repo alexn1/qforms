3a1cc1cfe864d442061bc9ce11d0cb953b920390
Alexander Nesterenko <alexn140@gmail.com>
2015-08-21 03:58:54 +0700

    package config

--- engine/www.js
+++ engine/www.js
@@ -3,11 +3,12 @@
 var http = require('http');
 var path = require('path');
 
+var pkg    = require('./package.json');
 var server = require('./server');
 var qforms = require('./qforms');
 
-var port = qforms.helper.getCommandLineParams().port || 3000;
-var host = qforms.helper.getCommandLineParams().host || 'localhost';
+var port = qforms.helper.getCommandLineParams().port || pkg.config.port;
+var host = qforms.helper.getCommandLineParams().host || pkg.config.host;
 
 var www = http.createServer(server).listen(port, host, function() {
     console.log('QForms server listening on http://{host}:{port}, applications from {appsDirPath}'.template({
