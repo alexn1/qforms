b0a814f54ef2048e37808e2f6d337ff1da8cc5c8
Alexander Nesterenko <alexn140@gmail.com>
2016-02-10 23:18:59 +0700

    refactoring

--- source/qforms/www.js
+++ source/qforms/www.js
@@ -7,22 +7,24 @@ var pkg    = require('./package.json');
 var qforms = require('./qforms');
 var server = require('./server');
 
-var port = qforms.helper.getCommandLineParams().port || pkg.config.port;
-var host = qforms.helper.getCommandLineParams().host || pkg.config.host;
-
-var www = http.createServer(server);
-www.on('error', function(err) {
-    if (err.code === 'EADDRINUSE') {
-        console.error('Address {host}:{port} in use.'.template({host: host, port: port}));
-    } else {
-        console.error(err);
-    }
-});
-www.listen(port, host, function() {
-    console.log('QForms server v{version} listening on http://{host}:{port}, applications from {appsDirPath}'.template({
-        version    : pkg.version,
-        host       : host,
-        port       : port,
-        appsDirPath: path.resolve(server.get('appsDirPath'))
-    }));
-});
+////////////////////////////////////////////////////////////////////////////////////////////////////
+main(); function main() {
+    var port = qforms.helper.getCommandLineParams().port || pkg.config.port;
+    var host = qforms.helper.getCommandLineParams().host || pkg.config.host;
+    var www = http.createServer(server);
+    www.on('error', function(err) {
+        if (err.code === 'EADDRINUSE') {
+            console.error('Address {host}:{port} in use.'.template({host: host, port: port}));
+        } else {
+            console.error(err);
+        }
+    });
+    www.listen(port, host, function() {
+        console.log('QForms server v{version} listening on http://{host}:{port}, applications from {appsDirPath}'.template({
+            version    : pkg.version,
+            host       : host,
+            port       : port,
+            appsDirPath: path.resolve(server.get('appsDirPath'))
+        }));
+    });
+}
