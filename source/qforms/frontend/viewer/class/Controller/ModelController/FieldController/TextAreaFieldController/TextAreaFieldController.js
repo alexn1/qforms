d98bf5828001731e4935376855765b41fef52e07
Alexander Nesterenko <alexn140@gmail.com>
2016-07-22 12:58:32 +0700

    TextAreaFieldController edit

--- source/qforms/frontend/viewer/class/Controller/ModelController/FieldController/TextAreaFieldController/TextAreaFieldController.js
+++ source/qforms/frontend/viewer/class/Controller/ModelController/FieldController/TextAreaFieldController/TextAreaFieldController.js
@@ -5,4 +5,45 @@ QForms.inherit(TextAreaFieldController, FieldController);
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 function TextAreaFieldController(model, parent) {
     FieldController.call(this, model, parent);
+    this.eventChange = new QForms.Event(this);
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+TextAreaFieldController.prototype.fill = function(row, view) {
+    TextAreaFieldController.super_.prototype.fill.call(this, row, view);
+    if (this.model.form.data.class === 'RowForm') {
+        var self = this;
+        $(view).children().change(function() {
+            self.onChange(this);
+        });
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+TextAreaFieldController.prototype.isValid = function(view) {
+    var isValid = true;
+    if (this.model.data.notNull === 'true') {
+        isValid = view.firstElementChild.value !== undefined && view.firstElementChild.value !== null && view.firstElementChild.value !== '';
+    }
+    if (!isValid) {
+        view.firstElementChild.classList.add('error');
+    } else {
+        view.firstElementChild.classList.remove('error');
+    }
+    return isValid;
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+TextAreaFieldController.prototype.onChange = function (el) {
+    var view = el.parentNode;
+    if (this.isValid(view)) {
+        this.model.save(view.dbRow, this.getValue(view));
+        // event
+        var ea = new QForms.EventArg(this);
+        ea.view  = view;
+        ea.row   = view.dbRow;
+        ea.el    = el;
+        ea.field = this;
+        this.eventChange.fire(ea);
+    }
+};
