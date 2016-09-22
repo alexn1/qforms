937d1852c0becdf31c8c452654f911606a110cf8
Alexander Nesterenko <alexn140@gmail.com>
2016-09-22 23:34:44 +0700

    FieldController api doc & demo

---
+++ apps/demo/pages/Employees/forms/Employees/Employees.js
@@ -0,0 +1,11 @@
+QForms.inherit(EmployeesEmployeesController, TableFormController);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+function EmployeesEmployeesController() {
+    EmployeesEmployeesController.super_.apply(this, arguments);
+};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+EmployeesEmployeesController.prototype.init = function() {
+    EmployeesEmployeesController.super_.prototype.init.call(this);
+};
