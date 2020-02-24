82b2ea8282dd41b4c7ee5c193ac561d4e4a9fb45
Alexander Nesterenko <alexn140@gmail.com>
2020-02-24 20:27:30 +0700

    ImageField

--- src/backend/viewer/Model/Field/ImageField/ImageField.js
+++ src/backend/viewer/Model/Field/ImageField/ImageField.js
@@ -1,7 +1,5 @@
 'use strict';
 
-module.exports = ImageField;
-
 var util    = require('util');
 var path    = require('path');
 var Promise = require('bluebird');
@@ -9,20 +7,25 @@ var Promise = require('bluebird');
 var server = require('../../../../../server');
 var Field  = require('../Field');
 
-util.inherits(ImageField, Field);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class ImageField extends Field {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function ImageField(data, parent) {
-    var self = this;
-    ImageField.super_.call(self, data, parent);
-    self.viewFilePath = path.join(
-        server.get('public'),
-        'viewer/class/Controller/ModelController/FieldController/ImageFieldController/view',
-        self.parent.data['@class'] + self.data['@class'] + 'View.ejs'
-    );
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(data, parent) {
+        super(data, parent);
+        var self = this;
+        self.viewFilePath = path.join(
+            server.get('public'),
+            'viewer/class/Controller/ModelController/FieldController/ImageFieldController/view',
+            self.parent.data['@class'] + self.data['@class'] + 'View.ejs'
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static async create(data, parent) {
+        return new ImageField(data, parent);
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-ImageField.create = function(data, parent) {
-    return Promise.resolve(new ImageField(data, parent));
-};
+module.exports = ImageField;
