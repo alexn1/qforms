3c9b8b7be60cd9a98e74c510d0427d03c7a311d9
Alexander Nesterenko <alexn140@gmail.com>
2020-02-25 00:29:31 +0700

    TextAreaFieldEditor

--- src/backend/editor/Editor/FieldEditor/TextAreaFieldEditor/TextAreaFieldEditor.js
+++ src/backend/editor/Editor/FieldEditor/TextAreaFieldEditor/TextAreaFieldEditor.js
@@ -1,41 +1,44 @@
 'use strict';
 
-module.exports = TextAreaFieldEditor;
-
 var util = require('util');
 var path = require('path');
 
 var server      = require('../../../../../server');
 var FieldEditor = require('../FieldEditor');
 
-util.inherits(TextAreaFieldEditor, FieldEditor);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class TextAreaFieldEditor extends FieldEditor {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function TextAreaFieldEditor(formEditor, name) {
-    var self = this;
-    TextAreaFieldEditor.super_.call(self, formEditor, name);
-    self.defaultViewDirPath = path.join(
-        server.get('public'),
-        'viewer/class/Controller/ModelController/FieldController/TextAreaFieldController/view'
-    );
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(formEditor, name) {
+        super(formEditor, name);
+        var self = this;
+        self.defaultViewDirPath = path.join(
+            server.get('public'),
+            'viewer/class/Controller/ModelController/FieldController/TextAreaFieldController/view'
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static createData(params) {
+        return {
+            '@class'      : 'TextAreaField',
+            '@attributes' : {
+                'name'        : params['name'],
+                'caption'     : params['caption']      ? params['caption']      : params['name'],
+                'isVisible'   : params['isVisible']    ? params['isVisible']    :         'true',
+                'width'       : params['width']        ? params['width']        :            '0',
+                'defaultValue': params['defaultValue'] ? params['defaultValue'] :             '',
+                'column'      : params['column']       ? params['column']       : params['name'],
+                'readOnly'    : params['readOnly']     ? params['readOnly']     :        'true',
+                'notNull'     : params['notNull']      ? params['notNull']      :        'false',
+                'align'       : params['align']        ? params['align']        :         'left',
+                'rows'        : params['rows']         ? params['rows']         :             '',
+                'cols'        : params['cols']         ? params['cols']         :             ''
+            }
+        };
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-TextAreaFieldEditor.createData = function(params) {
-    return {
-        '@class'      : 'TextAreaField',
-        '@attributes' : {
-            'name'        : params['name'],
-            'caption'     : params['caption']      ? params['caption']      : params['name'],
-            'isVisible'   : params['isVisible']    ? params['isVisible']    :         'true',
-            'width'       : params['width']        ? params['width']        :            '0',
-            'defaultValue': params['defaultValue'] ? params['defaultValue'] :             '',
-            'column'      : params['column']       ? params['column']       : params['name'],
-            'readOnly'    : params['readOnly']     ? params['readOnly']     :        'true',
-            'notNull'     : params['notNull']      ? params['notNull']      :        'false',
-            'align'       : params['align']        ? params['align']        :         'left',
-            'rows'        : params['rows']         ? params['rows']         :             '',
-            'cols'        : params['cols']         ? params['cols']         :             ''
-        }
-    };
-};
+module.exports = TextAreaFieldEditor;
