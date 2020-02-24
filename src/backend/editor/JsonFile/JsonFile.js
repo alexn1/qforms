539f9d4710c4e41ce704100796fa64a13ca23c21
Alexander Nesterenko <alexn140@gmail.com>
2020-02-24 23:00:48 +0700

    JsonFile

--- src/backend/editor/JsonFile/JsonFile.js
+++ src/backend/editor/JsonFile/JsonFile.js
@@ -1,68 +1,73 @@
 'use strict';
 
-module.exports = JsonFile;
-
 var fs      = require('fs');
 var qforms  = require('../../../qforms');
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function JsonFile(filePath) {
-    var self = this;
-    self.filePath = filePath;
-    self.content  = null;
-    self.data     = null;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class JsonFile {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(filePath) {
+        var self = this;
+        self.filePath = filePath;
+        self.content  = null;
+        self.data     = null;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    create() {
+        var self = this;
+        return qforms.Helper.exists(self.filePath).then(function (exists) {
+            if (exists) {
+                throw new Error('File {filePath} already exists'.replace('{filePath}', self.filePath));
+            } else {
+                if (self.data) {
+                } else if (self.content) {
+                    self.data = JSON.parse(self.content);
+                } else {
+                    self.data = {};
+                }
+                self.content = JSON.stringify(self.data, null, 4);
+                return qforms.Helper.writeFile(self.filePath, self.content);
+            }
+        });
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    read() {
+        var self = this;
+        return qforms.Helper.readFile(self.filePath).then(function (content) {
+            self.content = content;
+            self.data    = JSON.parse(content);
+        });
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    setAttr(name, value) {
+        var self = this;
+        self.data['@attributes'][name] = value;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    getAttr(name) {
+        var self = this;
+        return self.data['@attributes'][name];
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    save() {
+        var self = this;
+        console.log('JsonFile.prototype.save');
+        self.content = JSON.stringify(self.data, null, 4);
+        return qforms.Helper.writeFile(self.filePath, self.content);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    getData() {
+        var self = this;
+        return self.data;
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-JsonFile.prototype.create = function() {
-    var self = this;
-    return qforms.Helper.exists(self.filePath).then(function (exists) {
-        if (exists) {
-            throw new Error('File {filePath} already exists'.replace('{filePath}', self.filePath));
-        } else {
-            if (self.data) {
-            } else if (self.content) {
-                self.data = JSON.parse(self.content);
-            } else {
-                self.data = {};
-            }
-            self.content = JSON.stringify(self.data, null, 4);
-            return qforms.Helper.writeFile(self.filePath, self.content);
-        }
-    });
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-JsonFile.prototype.read = function() {
-    var self = this;
-    return qforms.Helper.readFile(self.filePath).then(function (content) {
-        self.content = content;
-        self.data    = JSON.parse(content);
-    });
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-JsonFile.prototype.setAttr = function(name, value) {
-    var self = this;
-    self.data['@attributes'][name] = value;
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-JsonFile.prototype.getAttr = function(name) {
-    var self = this;
-    return self.data['@attributes'][name];
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-JsonFile.prototype.save = function() {
-    var self = this;
-    console.log('JsonFile.prototype.save');
-    self.content = JSON.stringify(self.data, null, 4);
-    return qforms.Helper.writeFile(self.filePath, self.content);
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-JsonFile.prototype.getData = function() {
-    var self = this;
-    return self.data;
-};
+module.exports = JsonFile;
