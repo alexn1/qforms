28b2845d254ba9b4cd7f3c43f0d1d3e48faecce2
Alexander Nesterenko <alexn140@gmail.com>
2020-02-25 00:31:06 +0700

    TextBoxFieldEditor

--- src/backend/editor/Editor/FieldEditor/TextBoxFieldEditor/TextBoxFieldEditor.js
+++ src/backend/editor/Editor/FieldEditor/TextBoxFieldEditor/TextBoxFieldEditor.js
@@ -1,40 +1,43 @@
 'use strict';
 
-module.exports = TextBoxFieldEditor;
-
 var util = require('util');
 var path = require('path');
 
 var server      = require('../../../../../server');
 var FieldEditor = require('../FieldEditor');
 
-util.inherits(TextBoxFieldEditor, FieldEditor);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class TextBoxFieldEditor extends FieldEditor {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function TextBoxFieldEditor(formEditor, name) {
-    var self = this;
-    TextBoxFieldEditor.super_.call(self, formEditor, name);
-    self.defaultViewDirPath = path.join(
-        server.get('public'),
-        'viewer/class/Controller/ModelController/FieldController/TextBoxFieldController/view'
-    );
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(formEditor, name) {
+        super(formEditor, name);
+        var self = this;
+        self.defaultViewDirPath = path.join(
+            server.get('public'),
+            'viewer/class/Controller/ModelController/FieldController/TextBoxFieldController/view'
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static createData(params) {
+        return {
+            '@class'     : 'TextBoxField',
+            '@attributes': {
+                name          : params['name'],
+                caption       : params['caption']      ? params['caption']      : params['name'],
+                isVisible     : params['isVisible']    ? params['isVisible']    :         'true',
+                width         : params['width']        ? params['width']        :            '0',
+                defaultValue  : params['defaultValue'] ? params['defaultValue'] :             '',
+                value         : params['value']        ? params['value']        :             '',
+                column        : params['column']       ? params['column']       : params['name'],
+                readOnly      : params['readOnly']     ? params['readOnly']     :        'true',
+                notNull       : params['notNull']      ? params['notNull']      :        'false',
+                align         : params['align']        ? params['align']        :         'left'
+            }
+        };
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-TextBoxFieldEditor.createData = function(params) {
-    return {
-        '@class'     : 'TextBoxField',
-        '@attributes': {
-            name          : params['name'],
-            caption       : params['caption']      ? params['caption']      : params['name'],
-            isVisible     : params['isVisible']    ? params['isVisible']    :         'true',
-            width         : params['width']        ? params['width']        :            '0',
-            defaultValue  : params['defaultValue'] ? params['defaultValue'] :             '',
-            value         : params['value']        ? params['value']        :             '',
-            column        : params['column']       ? params['column']       : params['name'],
-            readOnly      : params['readOnly']     ? params['readOnly']     :        'true',
-            notNull       : params['notNull']      ? params['notNull']      :        'false',
-            align         : params['align']        ? params['align']        :         'left'
-        }
-    };
-};
+module.exports = TextBoxFieldEditor;
