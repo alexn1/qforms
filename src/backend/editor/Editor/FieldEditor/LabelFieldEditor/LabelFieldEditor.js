a0096fe985552d16698c7cf4fc4c8bd84ac90b61
Alexander Nesterenko <alexn140@gmail.com>
2020-02-25 00:26:17 +0700

    LabelFieldEditor

--- src/backend/editor/Editor/FieldEditor/LabelFieldEditor/LabelFieldEditor.js
+++ src/backend/editor/Editor/FieldEditor/LabelFieldEditor/LabelFieldEditor.js
@@ -1,37 +1,40 @@
 'use strict';
 
-module.exports = LabelFieldEditor;
-
 var util = require('util');
 var path = require('path');
 
 var server      = require('../../../../../server');
 var FieldEditor = require('../FieldEditor');
 
-util.inherits(LabelFieldEditor, FieldEditor);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class LabelFieldEditor extends FieldEditor {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function LabelFieldEditor(formEditor, name) {
-    var self = this;
-    LabelFieldEditor.super_.call(self, formEditor, name);
-    self.defaultViewDirPath = path.join(
-        server.get('public'),
-        'viewer/class/Controller/ModelController/FieldController/LabelFieldController/view'
-    );
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(formEditor, name) {
+        super(formEditor, name);
+        var self = this;
+        self.defaultViewDirPath = path.join(
+            server.get('public'),
+            'viewer/class/Controller/ModelController/FieldController/LabelFieldController/view'
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static createData(params) {
+        return {
+            '@class'     : 'LabelField',
+            '@attributes': {
+                'name'        : params['name'],
+                'caption'     : params['caption']      ? params['caption']      : params['name'],
+                'isVisible'   : params['isVisible']    ? params['isVisible']    :         'true',
+                'width'       : params['width']        ? params['width']        :            '0',
+                'defaultValue': params['defaultValue'] ? params['defaultValue'] :             '',
+                'column'      : params['column']       ? params['column']       : params['name'],
+                'align'       : params['align']        ? params['align']        :         'left'
+            }
+        };
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-LabelFieldEditor.createData = function(params) {
-    return {
-        '@class'     : 'LabelField',
-        '@attributes': {
-            'name'        : params['name'],
-            'caption'     : params['caption']      ? params['caption']      : params['name'],
-            'isVisible'   : params['isVisible']    ? params['isVisible']    :         'true',
-            'width'       : params['width']        ? params['width']        :            '0',
-            'defaultValue': params['defaultValue'] ? params['defaultValue'] :             '',
-            'column'      : params['column']       ? params['column']       : params['name'],
-            'align'       : params['align']        ? params['align']        :         'left'
-        }
-    };
-};
+module.exports = LabelFieldEditor;
