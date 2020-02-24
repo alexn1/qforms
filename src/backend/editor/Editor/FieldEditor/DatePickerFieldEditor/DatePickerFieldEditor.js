38ac6ec2aabe1dabbcf116a21b64477fc484fef0
Alexander Nesterenko <alexn140@gmail.com>
2020-02-25 00:21:03 +0700

    DatePickerFieldEditor

--- src/backend/editor/Editor/FieldEditor/DatePickerFieldEditor/DatePickerFieldEditor.js
+++ src/backend/editor/Editor/FieldEditor/DatePickerFieldEditor/DatePickerFieldEditor.js
@@ -1,39 +1,42 @@
 'use strict';
 
-module.exports = DatePickerFieldEditor;
-
 var util = require('util');
 var path = require('path');
 
 var server      = require('../../../../../server');
 var FieldEditor = require('../FieldEditor');
 
-util.inherits(DatePickerFieldEditor, FieldEditor);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class DatePickerFieldEditor extends FieldEditor {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function DatePickerFieldEditor(formEditor, name) {
-    var self = this;
-    DatePickerFieldEditor.super_.call(self, formEditor, name);
-    self.defaultViewDirPath = path.join(
-        server.get('public'),
-        'viewer/class/Controller/ModelController/FieldController/DatePickerFieldController/view'
-    );
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(formEditor, name) {
+        super(formEditor, name);
+        var self = this;
+        self.defaultViewDirPath = path.join(
+            server.get('public'),
+            'viewer/class/Controller/ModelController/FieldController/DatePickerFieldController/view'
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static createData(params) {
+        return {
+            '@class'     : 'DatePickerField',
+            '@attributes': {
+                'name'        : params['name'],
+                'caption'     : params['caption']      ? params['caption']      : params['name'],
+                'isVisible'   : params['isVisible']    ? params['isVisible']    :         'true',
+                'width'       : params['width']        ? params['width']        :            '0',
+                'defaultValue': params['defaultValue'] ? params['defaultValue'] :             '',
+                'column'      : params['column']       ? params['column']       : params['name'],
+                'readOnly'    : params['readOnly']     ? params['readOnly']     :        'true',
+                'notNull'     : params['notNull']      ? params['notNull']      :        'false',
+                'align'       : params['align']        ? params['align']        :         'left'
+            }
+        };
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-DatePickerFieldEditor.createData = function(params) {
-    return {
-        '@class'     : 'DatePickerField',
-        '@attributes': {
-            'name'        : params['name'],
-            'caption'     : params['caption']      ? params['caption']      : params['name'],
-            'isVisible'   : params['isVisible']    ? params['isVisible']    :         'true',
-            'width'       : params['width']        ? params['width']        :            '0',
-            'defaultValue': params['defaultValue'] ? params['defaultValue'] :             '',
-            'column'      : params['column']       ? params['column']       : params['name'],
-            'readOnly'    : params['readOnly']     ? params['readOnly']     :        'true',
-            'notNull'     : params['notNull']      ? params['notNull']      :        'false',
-            'align'       : params['align']        ? params['align']        :         'left'
-        }
-    };
-};
+module.exports = DatePickerFieldEditor;
