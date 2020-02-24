55e644cc1007385a580623db1a96020986b3aeac
Alexander Nesterenko <alexn140@gmail.com>
2020-02-24 20:46:01 +0700

    TableForm

--- src/backend/viewer/Model/Form/TableForm/TableForm.js
+++ src/backend/viewer/Model/Form/TableForm/TableForm.js
@@ -1,7 +1,5 @@
 'use strict';
 
-module.exports = TableForm;
-
 var util    = require('util');
 var path    = require('path');
 var Promise = require('bluebird');
@@ -9,20 +7,25 @@ var Promise = require('bluebird');
 var server = require('../../../../../server');
 var Form   = require('../Form');
 
-util.inherits(TableForm, Form);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class TableForm extends Form {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function TableForm(data, parent) {
-    var self = this;
-    TableForm.super_.call(self, data, parent);
-    self.viewFilePath = path.join(
-        server.get('public'),
-        'viewer/class/Controller/ModelController/FormController/TableFormController/view',
-        self.data['@class'] + 'View.ejs'
-    );
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(data, parent) {
+        super(data, parent);
+        var self = this;
+        self.viewFilePath = path.join(
+            server.get('public'),
+            'viewer/class/Controller/ModelController/FormController/TableFormController/view',
+            self.data['@class'] + 'View.ejs'
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static async create(data, parent) {
+        return new TableForm(data, parent);
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-TableForm.create = function(data, parent) {
-    return Promise.resolve(new TableForm(data, parent));
-};
+module.exports = TableForm;
