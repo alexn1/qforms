b2dd1778e445baa7cf9a768187ae49a4a592e7ab
Alexander Nesterenko <alexn140@gmail.com>
2020-02-25 00:27:44 +0700

    LinkFieldEditor

--- src/backend/editor/Editor/FieldEditor/LinkFieldEditor/LinkFieldEditor.js
+++ src/backend/editor/Editor/FieldEditor/LinkFieldEditor/LinkFieldEditor.js
@@ -1,38 +1,41 @@
 'use strict';
 
-module.exports = LinkFieldEditor;
-
 var util = require('util');
 var path = require('path');
 
 var server      = require('../../../../../server');
 var FieldEditor = require('../FieldEditor');
 
-util.inherits(LinkFieldEditor, FieldEditor);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class LinkFieldEditor extends FieldEditor {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function LinkFieldEditor(formEditor, name) {
-    var self = this;
-    LinkFieldEditor.super_.call(self, formEditor, name);
-    self.defaultViewDirPath = path.join(
-        server.get('public'),
-        'viewer/class/Controller/ModelController/FieldController/LinkFieldController/view'
-    );
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(formEditor, name) {
+        super(formEditor, name);
+        var self = this;
+        self.defaultViewDirPath = path.join(
+            server.get('public'),
+            'viewer/class/Controller/ModelController/FieldController/LinkFieldController/view'
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static createData(params) {
+        return {
+            '@class' : 'LinkField',
+            '@attributes': {
+                'name':params['name'],
+                'caption'     : params['caption']      ? params['caption']      : params['name'],
+                'isVisible'   : params['isVisible']    ? params['isVisible']    :         'true',
+                'width'       : params['width']        ? params['width']        :            '0',
+                'defaultValue': params['defaultValue'] ? params['defaultValue'] :             '',
+                'column'      : params['column']       ? params['column']       : params['name'],
+                'readOnly'    : params['readOnly']     ? params['readOnly']     :        'true',
+                'notNull'     : params['notNull']      ? params['notNull']      :        'false'
+            }
+        };
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-LinkFieldEditor.createData = function(params) {
-    return {
-        '@class' : 'LinkField',
-        '@attributes': {
-            'name':params['name'],
-            'caption'     : params['caption']      ? params['caption']      : params['name'],
-            'isVisible'   : params['isVisible']    ? params['isVisible']    :         'true',
-            'width'       : params['width']        ? params['width']        :            '0',
-            'defaultValue': params['defaultValue'] ? params['defaultValue'] :             '',
-            'column'      : params['column']       ? params['column']       : params['name'],
-            'readOnly'    : params['readOnly']     ? params['readOnly']     :        'true',
-            'notNull'     : params['notNull']      ? params['notNull']      :        'false'
-        }
-    };
-};
+module.exports = LinkFieldEditor;
