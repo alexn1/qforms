ff14c3702d629813a876d052d07a0ea364e610cf
Alexander Nesterenko <alexn140@gmail.com>
2020-02-25 00:19:35 +0700

    ComboBoxFieldEditor

--- src/backend/editor/Editor/FieldEditor/ComboBoxFieldEditor/ComboBoxFieldEditor.js
+++ src/backend/editor/Editor/FieldEditor/ComboBoxFieldEditor/ComboBoxFieldEditor.js
@@ -1,41 +1,44 @@
 'use strict';
 
-module.exports = ComboBoxFieldEditor;
-
 var util = require('util');
 var path = require('path');
 
 var server      = require('../../../../../server');
 var FieldEditor = require('../FieldEditor');
 
-util.inherits(ComboBoxFieldEditor, FieldEditor);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class ComboBoxFieldEditor extends FieldEditor {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function ComboBoxFieldEditor(formEditor, name) {
-    var self = this;
-    ComboBoxFieldEditor.super_.call(self, formEditor, name);
-    self.defaultViewDirPath = path.join(
-        server.get('public'),
-        'viewer/class/Controller/ModelController/FieldController/ComboBoxFieldController/view'
-    );
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(formEditor, name) {
+        super(formEditor, name);
+        var self = this;
+        self.defaultViewDirPath = path.join(
+            server.get('public'),
+            'viewer/class/Controller/ModelController/FieldController/ComboBoxFieldController/view'
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static createData(params) {
+        return {
+            '@class'     : 'ComboBoxField',
+            '@attributes': {
+                'name'          :params['name'],
+                'caption'       :params['caption']        ? params['caption']        : params['name'],
+                'isVisible'     :params['isVisible']      ? params['isVisible']      :         'true',
+                'width'         :params['width']          ? params['width']          :            '0',
+                'defaultValue'  :params['defaultValue']   ? params['defaultValue']   :             '',
+                'column'        :params['column']         ? params['column']         : params['name'],
+                'readOnly'      :params['readOnly']       ? params['readOnly']       :        'true',
+                'notNull'       :params['notNull']        ? params['notNull']        :        'false',
+                'dataSourceName':params['dataSourceName'] ? params['dataSourceName'] :             '',
+                'valueColumn'   :params['valueColumn']    ? params['valueColumn']    :             '',
+                'displayColumn' :params['displayColumn']  ? params['displayColumn']  :             ''
+            }
+        };
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-ComboBoxFieldEditor.createData = function(params) {
-    return {
-        '@class'     : 'ComboBoxField',
-        '@attributes': {
-            'name'          :params['name'],
-            'caption'       :params['caption']        ? params['caption']        : params['name'],
-            'isVisible'     :params['isVisible']      ? params['isVisible']      :         'true',
-            'width'         :params['width']          ? params['width']          :            '0',
-            'defaultValue'  :params['defaultValue']   ? params['defaultValue']   :             '',
-            'column'        :params['column']         ? params['column']         : params['name'],
-            'readOnly'      :params['readOnly']       ? params['readOnly']       :        'true',
-            'notNull'       :params['notNull']        ? params['notNull']        :        'false',
-            'dataSourceName':params['dataSourceName'] ? params['dataSourceName'] :             '',
-            'valueColumn'   :params['valueColumn']    ? params['valueColumn']    :             '',
-            'displayColumn' :params['displayColumn']  ? params['displayColumn']  :             ''
-        }
-    };
-};
+module.exports = ComboBoxFieldEditor;
