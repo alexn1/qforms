cd5261ff2be456d1929f7ec79f46a63b4464de70
Alexander Nesterenko <alexn140@gmail.com>
2020-02-24 20:23:44 +0700

    DatePickerField

--- src/backend/viewer/Model/Field/DatePickerField/DatePickerField.js
+++ src/backend/viewer/Model/Field/DatePickerField/DatePickerField.js
@@ -1,7 +1,5 @@
 'use strict';
 
-module.exports = DatePickerField;
-
 var util    = require('util');
 var path    = require('path');
 var Promise = require('bluebird');
@@ -9,20 +7,25 @@ var Promise = require('bluebird');
 var server = require('../../../../../server');
 var Field  = require('../Field');
 
-util.inherits(DatePickerField, Field);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class DatePickerField extends Field {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function DatePickerField(data, parent) {
-    var self = this;
-    DatePickerField.super_.call(self, data, parent);
-    self.viewFilePath = path.join(
-        server.get('public'),
-        'viewer/class/Controller/ModelController/FieldController/DatePickerFieldController/view',
-        self.parent.data['@class'] + self.data['@class'] + 'View.ejs'
-    );
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(data, parent) {
+        super(data, parent);
+        var self = this;
+        self.viewFilePath = path.join(
+            server.get('public'),
+            'viewer/class/Controller/ModelController/FieldController/DatePickerFieldController/view',
+            self.parent.data['@class'] + self.data['@class'] + 'View.ejs'
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static async create(data, parent) {
+        return new DatePickerField(data, parent);
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-DatePickerField.create = function(data, parent) {
-    return Promise.resolve(new DatePickerField(data, parent));
-};
+module.exports = DatePickerField;
