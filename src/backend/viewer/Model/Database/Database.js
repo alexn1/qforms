1d84cf8e7d669d25940cccb8147a2ec8e2f23165
Alexander Nesterenko <alexn140@gmail.com>
2020-02-24 20:13:22 +0700

    ref

--- src/backend/viewer/Model/Database/Database.js
+++ src/backend/viewer/Model/Database/Database.js
@@ -1,100 +1,90 @@
 'use strict';
 
-module.exports = Database;
-
 var util    = require('util');
 var Promise = require('bluebird');
-//var sqlish  = require('sqlish');
 
 var Model  = require('../Model');
 
-util.inherits(Database, Model);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Database extends Model {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Database.create = function(data, parent) {
-    throw new Error('Database.create not implemented');
-};
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(data, parent) {
+        super(data, parent);
+        //console.log('new Database');
+    }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function Database(data, parent) {
-    var self = this;
-    //console.log('new Database');
-    Database.super_.call(self, data, parent);
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static create(data, parent) {
+        throw new Error('Database.create not implemented');
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    async deinit() {
+        throw new Error('Database.prototype.deinit not implemented');
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    async getConnection(context) {
+        throw new Error('Database.prototype.getConnection not implemented');
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    async query(context, query, params, nest) {
+        throw new Error('Database.prototype.query not implemented');
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    async beginTransaction(cnn) {
+        throw new Error('Database.prototype.beginTransaction not implemented');
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    async commit(cnn) {
+        throw new Error('Database.prototype.commit not implemented');
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    async rollback(cnn, err) {
+        throw new Error('Database.prototype.rollback not implemented');
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    desc(context, table) {
+        throw new Error('Database.prototype.desc not implemented');
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    getUpdateQuery(tableName, values, where) {
+        console.log('Database.prototype.getUpdateQuery', tableName);
+        var valueKeys = Object.keys(values);
+        var whereKeys = Object.keys(where);
+        var valuesString = valueKeys.map(name => `${name} = {${name}}`).join(', ');
+        var whereString = whereKeys.map(name => `${name} = {${name}}`).join(' and ');
+        return `update ${tableName} set ${valuesString} where ${whereString}`;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    getInsertQuery(tableName, values) {
+        console.log('Database.prototype.getInsertQuery');
+        var columns = Object.keys(values);
+        const columnsString = columns.join(', ');
+        const valuesString = columns.map(column => `{${column}}`).join(', ');
+        const myQuery = `insert into ${tableName}(${columnsString}) values (${valuesString})`;
+        console.log('myQuery:', myQuery);
+        return myQuery;
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    getDeleteQuery(tableName, rowKeyValues) {
+        console.log('Database.prototype.getDeleteQuery');
+        const columns = Object.keys(rowKeyValues);
+        const whereString = columns.map(column => `${column} = {${column}}`).join(' and ');
+        var query2 = `delete from ${tableName} where ${whereString}`;
+        console.log('query2:', query2);
+        return query2;
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Database.prototype.deinit = function() {
-    throw new Error('Database.prototype.deinit not implemented');
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Database.prototype.getConnection = function(context) {
-    throw new Error('Database.prototype.getConnection not implemented');
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Database.prototype.query = function(context, query, params, nest) {
-    throw new Error('Database.prototype.query not implemented');
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Database.prototype.beginTransaction = function(cnn) {
-    throw new Error('Database.prototype.beginTransaction not implemented');
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Database.prototype.commit = function (cnn) {
-    throw new Error('Database.prototype.commit not implemented');
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Database.prototype.rollback = function (cnn, err) {
-    throw new Error('Database.prototype.rollback not implemented');
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Database.prototype.desc = function(context, table) {
-    throw new Error('Database.prototype.desc not implemented');
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Database.prototype.getUpdateQuery = function(tableName, values, where) {
-    console.log('Database.prototype.getUpdateQuery', tableName);
-    var self = this;
-    var valueKeys = Object.keys(values);
-    var whereKeys = Object.keys(where);
-    var valuesString = valueKeys.map(name => `${name} = {${name}}`).join(', ');
-    var whereString = whereKeys.map(name => `${name} = {${name}}`).join(' and ');
-    return `update ${tableName} set ${valuesString} where ${whereString}`;
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Database.prototype.getInsertQuery = function (tableName, values) {
-    console.log('Database.prototype.getInsertQuery');
-    //const query = new sqlish.Sqlish().insert(tableName, values).toString();
-    //console.log('query:', query);
-    //const query2 = query.replace(/"{/g,'{').replace(/}"/g,'}');
-    //console.log('query2:', query2);
-
-    var columns = Object.keys(values);
-    const columnsString = columns.join(', ');
-    const valuesString = columns.map(column => `{${column}}`).join(', ');
-    const myQuery = `insert into ${tableName}(${columnsString}) values (${valuesString})`;
-    console.log('myQuery:', myQuery);
-
-    return myQuery;
-};
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Database.prototype.getDeleteQuery = function (tableName, rowKeyValues) {
-    console.log('Database.prototype.getDeleteQuery');
-    //var query = new sqlish.Sqlish().deleteFrom(tableName).where(rowKeyValues).toString();
-    //console.log('query:', query);
-
-    const columns = Object.keys(rowKeyValues);
-    const whereString = columns.map(column => `${column} = {${column}}`).join(' and ');
-    var query2 = `delete from ${tableName} where ${whereString}`;
-    console.log('query2:', query2);
-
-    return query2;
-};
+module.exports = Database;
