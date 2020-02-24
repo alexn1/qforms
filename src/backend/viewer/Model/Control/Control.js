adbc68b3b700212681a005b7a442894e4a92dbcc
Alexander Nesterenko <alexn140@gmail.com>
2020-02-24 20:18:25 +0700

    Control

--- src/backend/viewer/Model/Control/Control.js
+++ src/backend/viewer/Model/Control/Control.js
@@ -1,24 +1,27 @@
 'use strict';
 
-module.exports = Control;
-
 var util    = require('util');
 var path    = require('path');
 var Promise = require('bluebird');
 
 var Model = require('../Model');
 
-util.inherits(Control, Model);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Control extends Model {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function Control(data, parent) {
-    var self = this;
-    Control.super_.call(self, data, parent);
-    self.dirPath            = path.join(parent.dirPath, self.name);
-    self.customViewFilePath = path.join(self.dirPath, self.name + '.ejs');
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(data, parent) {
+        super(data, parent);
+        var self = this;
+        self.dirPath            = path.join(parent.dirPath, self.name);
+        self.customViewFilePath = path.join(self.dirPath, self.name + '.ejs');
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    static async create(data, parent) {
+        return new Control(data, parent);
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-Control.create = function(data, parent) {
-    return Promise.resolve(new Control(data, parent));
-};
+module.exports = Control;
