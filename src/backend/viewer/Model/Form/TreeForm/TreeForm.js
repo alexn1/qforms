250819e4c8deef32fcb979685c883ef9c140de4c
Alexander Nesterenko <alexn140@gmail.com>
2020-02-24 20:47:49 +0700

    TreeForm

--- src/backend/viewer/Model/Form/TreeForm/TreeForm.js
+++ src/backend/viewer/Model/Form/TreeForm/TreeForm.js
@@ -1,7 +1,5 @@
 'use strict';
 
-module.exports = TreeForm;
-
 var util    = require('util');
 var path    = require('path');
 var Promise = require('bluebird');
@@ -9,20 +7,25 @@ var Promise = require('bluebird');
 var server = require('../../../../../server');
 var Form   = require('../Form');
 
-util.inherits(TreeForm, Form);
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class TreeForm extends Form {
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-function TreeForm(data, parent) {
-    var self = this;
-    TreeForm.super_.call(self, data, parent);
-    self.viewFilePath = path.join(
-        server.get('public'),
-        'viewer/class/Controller/ModelController/FormController/TreeFormController/view',
-        self.data['@class'] + 'View.ejs'
-    );
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    constructor(data, parent) {
+        super(data, parent);
+        var self = this;
+        self.viewFilePath = path.join(
+            server.get('public'),
+            'viewer/class/Controller/ModelController/FormController/TreeFormController/view',
+            self.data['@class'] + 'View.ejs'
+        );
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    static async create(data, parent) {
+        return new TreeForm(data, parent);
+    }
+
 }
 
-////////////////////////////////////////////////////////////////////////////////////////////////////
-TreeForm.create = function(data, parent) {
-    return Promise.resolve(new TreeForm(data, parent));
-};
+module.exports = TreeForm;
