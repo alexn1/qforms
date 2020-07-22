b6ff5b8b5bcb53ef527d65d3f4bd8186f2eae4e0
Alexander Nesterenko <alexn140@gmail.com>
2020-07-22 12:34:04 +0700

    mysql fix

--- tools/create.sql
+++ tools/create.sql
@@ -1,4 +1,4 @@
 create database demo charset utf8;
-create user 'demo'@'localhost' identified by '123qwe';
+create user 'demo'@'localhost' identified with mysql_native_password by '123qwe';
 grant all privileges on demo.* to 'demo'@'localhost';
 flush privileges;
