d846e08064c58b499ddeeeecd8f09d1170baabc1
Alexander Nesterenko <alexn140@gmail.com>
2020-09-10 23:55:58 +0700

    datepicker

--- gui/datepicker/datepicker.js
+++ gui/datepicker/datepicker.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('DOMContentLoaded');
-
-    const now = new Date();
-    now.setDate(1);
-    now.setDate(2 - now.getDay());
-
+    const date = new Date(2020, 9, 23);
+    date.setDate(1);                     // first day of month
+    date.setDate(2 - date.getDay());     // first day of table
+    const tds = document.querySelectorAll('td');
     for (let i = 0; i < 42; i++) {
-        console.log(now);
-        now.setDate(now.getDate() + 1);
+        tds[i].innerText = date.getDate().toString();
+        date.setDate(date.getDate() + 1);
     }
-
-
 });
