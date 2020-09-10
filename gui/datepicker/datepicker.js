ebea3930f5d64f79c36f091515024482ac3347a3
Alexander Nesterenko <alexn140@gmail.com>
2020-09-11 01:21:15 +0700

    datepicker

--- gui/datepicker/datepicker.js
+++ gui/datepicker/datepicker.js
@@ -1,11 +1,52 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded');
-    const date = new Date(2020, 9, 23);
+const months = [
+    'Январь', 'Февраль',
+    'Март', 'Апрель', 'Май',
+    'Июнь', 'Июль', 'Август',
+    'Сентябрь', 'Октябрь', 'Ноябрь',
+    'Декабрь'
+];
+
+function fillDatePicker(datepicker1, date) {
+    console.log('fillDatePicker', date);
+
+    const today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+    console.log('today:', today );
+    const month = date.getMonth();
+    const year = date.getFullYear();
     date.setDate(1);                     // first day of month
     date.setDate(2 - date.getDay());     // first day of table
-    const tds = document.querySelectorAll('td');
+
+    // caption
+    const caption = datepicker1.querySelector('caption');
+    caption.innerText = `${months[month]}, ${year}`;
+
+    // tds
+    const tds = datepicker1.querySelectorAll('td');
     for (let i = 0; i < 42; i++) {
         tds[i].innerText = date.getDate().toString();
+        if (date.getMonth() !== month) {
+            tds[i].classList.add('out');
+
+        } else {
+            tds[i].classList.remove('out');
+        }
+        if (date >= today) {
+            tds[i].classList.add('selectable');
+        } else {
+            tds[i].classList.remove('selectable');
+        }
+        if (date.getTime() === today.getTime()) {
+            tds[i].classList.add('today');
+        } else {
+            tds[i].classList.remove('today');
+        }
         date.setDate(date.getDate() + 1);
     }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOMContentLoaded');
+    const datepicker1 = document.getElementById('datepicker1');
+    const date = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+    fillDatePicker(datepicker1, date);
 });
