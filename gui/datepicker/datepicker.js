df1d0ba8440ebc185a9debabd8f8e57b0726b49c
Alexander Nesterenko <alexn140@gmail.com>
2020-09-11 21:31:57 +0700

    DropdownDatePicker class

--- gui/datepicker/datepicker.js
+++ gui/datepicker/datepicker.js
@@ -9,7 +9,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 function onClick(event) {
-    const dropdownContent = event.target.parentElement.querySelector('.dropdown-content');
+    const dropdownContent = event.target.parentElement.querySelector('table.DatePicker');
+    console.log('dropdownContent:', dropdownContent);
     if (dropdownContent) {
         dropdownContent.classList.toggle("show");
     }
@@ -17,23 +18,9 @@ function onClick(event) {
 
 function onBlur(event) {
     console.log('onBlur', event);
-    const dropdownContent = event.target.parentElement.querySelector('.dropdown-content');
+    const dropdownContent = event.target.parentElement.querySelector('.DatePicker');
     if (dropdownContent && dropdownContent.classList.contains('show')) {
         dropdownContent.classList.remove('show');
     }
 }
 
-/*
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn') && !event.target.matches('.dropinput')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
-    }
-}*/
