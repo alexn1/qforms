af340ae49df6463d4a94805bd3f9d147dae89273
Alexander Nesterenko <alexn140@gmail.com>
2020-09-12 00:56:22 +0700

    second date picker

--- gui/datepicker/datepicker.js
+++ gui/datepicker/datepicker.js
@@ -2,6 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('DOMContentLoaded');
     const dropdownDatePicker1 = new DropdownDatePicker(document.getElementById('dropdownDatePicker1'));
     dropdownDatePicker1.init();
+
+    const dropdownDatePicker2 = new DropdownDatePicker(document.getElementById('dropdownDatePicker2'));
+    dropdownDatePicker2.init();
 });
 
 
