903458d1e71868dc519f43e46d500e34eb81405e
Alexander Nesterenko <alexn140@gmail.com>
2020-09-14 00:21:27 +0700

    blur

--- src/frontend/common/class/Widget/MenuWidget/MenuWidget.js
+++ src/frontend/common/class/Widget/MenuWidget/MenuWidget.js
@@ -5,12 +5,21 @@ class MenuWidget extends Widget {
         console.log('MenuWidget.constructor', el);
         super(el);
         const buttons = this.el.querySelectorAll('.dropdown > button');
-        buttons.forEach(button => button.addEventListener('click', this.onDropdownButtonClick.bind(this)));
+        buttons.forEach(button => {
+            button.addEventListener('click', this.onDropdownButtonClick.bind(this));
+            button.addEventListener('blur', this.onDropdownButtonBlur.bind(this));
+        });
     }
 
     onDropdownButtonClick(event) {
         console.log('onDropdownButtonClick:', event.target);
         const dropdownContent = event.target.parentElement.querySelector('.dropdown-content');
         dropdownContent.classList.add('show');
+    }
+
+    onDropdownButtonBlur(event) {
+        console.log('onDropdownButtonBlur:', event.target);
+        const dropdownContent = event.target.parentElement.querySelector('.dropdown-content');
+        dropdownContent.classList.remove('show');
     }
 }
