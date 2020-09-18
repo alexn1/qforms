6f4c83c403152d6022fde7ec2dbbb77d8b4f836b
Alexander Nesterenko <alexn140@gmail.com>
2020-09-18 16:54:26 +0700

    modal works

--- gui/modal/modal.js
+++ gui/modal/modal.js
@@ -1,3 +1,15 @@
+
 function onFirstClick() {
     console.log('onFirstClick');
+    document.querySelector(".modal").style.display = "block";
+}
+
+function onCloseClick() {
+    console.log('onCloseClick');
+    document.querySelector(".modal").style.display = "none";
+}
+
+function onModalClick() {
+    console.log('onModalClick');
+    // document.querySelector(".modal").style.display = "none";
 }
