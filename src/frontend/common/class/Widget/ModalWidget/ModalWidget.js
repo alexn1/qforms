e206d546a64a76410cbbc6c6dbe2d59467b39074
Alexander Nesterenko <alexn140@gmail.com>
2020-09-19 02:40:08 +0700

    close modal

--- src/frontend/common/class/Widget/ModalWidget/ModalWidget.js
+++ src/frontend/common/class/Widget/ModalWidget/ModalWidget.js
@@ -4,6 +4,16 @@ class ModalWidget extends Widget {
     constructor(el) {
         console.log('ModalWidget.constructor', el);
         super(el);
+        this.getCloseElement().addEventListener('click', this.onCloseClick.bind(this))
+    }
+
+    onCloseClick() {
+        console.log('ModalWidget.onCloseClick');
+        this.el.style.display = "none";
+    }
+
+    getCloseElement() {
+        return this.el.querySelector('span.close');
     }
 
     open() {
