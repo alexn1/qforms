1b9de6b15fd575bbc9625a5c7058ebdda1c51b4d
Alexander Nesterenko <alexn140@gmail.com>
2020-09-20 17:51:06 +0700

    formatDate

--- gui/test/test.js
+++ gui/test/test.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('DOMContentLoaded');
 
-    // Аналог $(document).ready(function(){
+    // Аналог $(document).ready(function() {
     // Если должен быть найден один элемент
     // if ((e = document.querySelector("#form_error_message_frontend + div > div:last-child label")) !== null) {
     //     e.classList.add('last'); // Аналог выборки и присвоения класса
@@ -29,18 +29,34 @@ document.addEventListener('DOMContentLoaded', function() {
     box.onmouseup = () => {
         console.log('up');
     }
-    const textarea = document.querySelector('textarea');
-
+    /*const textarea = document.querySelector('textarea');
     const isoString = '2020-09-04T17:00:00.000Z';
     const date = new Date(isoString);
     console.log('date:', date);
-
     console.log('date.toString():', date.toString());
     console.log('JSON.stringify(date):', JSON.stringify(date));
     console.log('date.toISOString():', date.toISOString());
     console.log('date instanceof Date', date instanceof Date);
-
     // textarea.value = JSON.stringify({field: date});
-    textarea.value = date.toISOString();
+    textarea.value = date.toISOString();*/
+
+    console.log('date:', formatDate(new Date(), '{YYYY}-{MM}-{DD}'));
+    console.log('date:', formatDate(new Date(), '{DD}.{MM}.{YYYY} {hh}:{mm}:{ss}'));
 
 });
+
+function formatDate(date, format) {
+    const YYYY = date.getFullYear();
+    const M    = date.getMonth();
+    const D    = date.getDate();
+    const h    = date.getHours();
+    const m    = date.getMinutes();
+    const s    = date.getSeconds();
+    const MM = M < 10 ? `0${M}` : M;
+    const DD = D < 10 ? `0${D}` : D;
+    const hh = h < 10 ? `0${h}` : h;
+    const mm = m < 10 ? `0${m}` : m;
+    const ss = s < 10 ? `0${s}` : s;
+    const values = {YYYY, M, D, h, m, s, MM, DD, hh, mm, ss};
+    return format.replace(/\{([\w\.]+)\}/g, (text, name) => values[name] ? values[name] : text);
+}
