6e7d6cea491a023054cdd997deb570ec2cf80bc4
Alexander Nesterenko <alexn140@gmail.com>
2020-09-22 00:35:45 +0700

    dropdown button

--- gui/dropdownbutton/dropdownbutton.js
+++ gui/dropdownbutton/dropdownbutton.js
@@ -1,16 +1,58 @@
 class DropdownButton {
     constructor(el) {
-        console.log('DropdownButton.constructor');
+        // console.log('DropdownButton.constructor');
         this.el = el;
+        this.onClick = null;
         this.getButtonElement().addEventListener('click', this.onButtonClick.bind(this));
+        this.getButtonElement().addEventListener('blur', this.onButtonBlur.bind(this));
+        this.getDivElement().addEventListener('mousedown', this.onDivMousedown.bind(this));
+        this.getDivElement().addEventListener('click', this.onDivClick.bind(this));
+    }
+
+    onDivClick(e) {
+        console.log('DropdownButton.onDivClick', e.target);
+        this.hide();
+        if (this.onClick) {
+            this.onClick(e.target);
+        }
+    }
+
+    onDivMousedown(e) {
+        // console.log('DropdownButton.onDivMousedown');
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
     }
 
     getButtonElement() {
-        return this.el.querySelector('button');
+        return this.el.querySelector('div.DropdownButton > button');
+    }
+
+    getDivElement() {
+        return this.el.querySelector('div.DropdownButton > div');
     }
 
     onButtonClick() {
         console.log('DropdownButton.onButtonClick');
+        this.show();
+    }
+
+    onButtonBlur() {
+        // console.log('DropdownButton.onButtonBlur');
+        if (this.isShown()) {
+            this.hide();
+        }
+    }
+
+    isShown() {
+        return this.el.classList.contains('show');
+    }
+
+    hide() {
+        this.el.classList.remove('show');
+    }
+
+    show() {
         this.el.classList.toggle('show');
     }
 }
