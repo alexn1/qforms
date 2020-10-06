45fc1fc49c89c85ce15058f0d47058fc28626427
Alexander Nesterenko <alexn140@gmail.com>
2020-10-07 00:11:58 +0700

    toggle

--- src/frontend/common/jsx/DropdownButton/DropdownButton.jsx
+++ src/frontend/common/jsx/DropdownButton/DropdownButton.jsx
@@ -2,11 +2,20 @@ class DropdownButton extends React.Component{
     constructor(props) {
         super(props);
         if (props.cb) props.cb(this);
+        this.state = {
+            open: false
+        };
+    }
+    onClick = e => {
+        console.log('DropdownButton.onClick', this.state);
+        this.setState(state => ({
+            open: !state.open
+        }));
     }
     render() {
         return (
-            <div className="DropdownButton">
-                <button>Actions</button>
+            <div className={`DropdownButton ${this.state.open && 'show'}`}>
+                <button onClick={this.onClick}>Actions</button>
                 <ul>
                     {this.props.actions.map(action => <li key={action.name} data-action={action.name}>{action.title}</li>)}
                 </ul>
