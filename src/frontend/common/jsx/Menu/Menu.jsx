962b3509b226bdaa24a7024b12d3e76c0967c66f
Alexander Nesterenko <alexn140@gmail.com>
2020-10-07 10:13:28 +0700

    ref

--- src/frontend/common/jsx/Menu/Menu.jsx
+++ src/frontend/common/jsx/Menu/Menu.jsx
@@ -4,50 +4,45 @@ class Menu extends React.Component {
         super(props);
         if (this.props.cb) this.props.cb(this);
         this.state = {};
-        this.onMenuClick     = this.onMenuClick.bind(this);
-        this.onBlur          = this.onBlur.bind(this);
-        this.onMouseDown     = this.onMouseDown.bind(this);
-        this.onMenuItemClick = this.onMenuItemClick.bind(this);
     }
-    onMenuClick(e) {
+    onMenuClick = async (e) => {
         // console.log('Menu.onMenuClick', e.currentTarget.dataset.menu);
-        this.toggleMenu(e.currentTarget.dataset.menu);
+        await this.toggleMenu(e.currentTarget.dataset.menu);
     }
-    onBlur(e) {
+    onBlur = async (e) => {
         // console.log('Menu.onBlur', e.currentTarget.dataset.menu);
-        this.closeMenu(e.currentTarget.dataset.menu);
+        await this.closeMenu(e.currentTarget.dataset.menu);
     }
     toggleMenu(menu) {
-        // console.log('Menu.toggleMenu', menu);
-        this.setState(prevState => ({
-            [menu]: !prevState[menu]
-        }));
+        return new Promise(resolve => {
+            this.setState(prevState => ({
+                [menu]: !prevState[menu]
+            }), resolve);
+        });
     }
     closeMenu(menu) {
-        this.setState({[menu]: false});
+        return new Promise(resolve => this.setState({[menu]: false}, resolve));
     }
-    onMouseDown(e) {
+    onMouseDown = (e) => {
         // console.log('Menu.onMouseDown');
         e.preventDefault();
         // e.stopPropagation();
         // return false;
     }
-    onMenuItemClick(e) {
-        // console.log('Menu.onMenuItemClick', e.currentTarget.dataset.menu, e.currentTarget.dataset.item);
-        this.closeMenu(e.currentTarget.dataset.menu);
+    onMenuItemClick = async (e) => {
+        // console.log('Menu.onMenuItemClick', e.target.dataset.menu, e.target.dataset.item);
+        e.persist();
+        await this.closeMenu(e.target.dataset.menu);
         if (this.props.onClick) {
-            this.props.onClick(e.currentTarget.dataset.menu, e.currentTarget.dataset.item);
+            this.props.onClick(e.target.dataset.menu, e.target.dataset.item);
         }
     }
     render() {
         return this.props.items.map(menu => <div key={menu.name} className={this.state[menu.name] ? 'active' : ''}>
             <button data-menu={menu.name} onClick={this.onMenuClick} onBlur={this.onBlur}>{menu.title}</button>
-            <div>
+            <div onMouseDown={this.onMouseDown} onClick={this.onMenuItemClick}>
                 {menu.items.map(item =>
-                    <a key={item.name} data-menu={menu.name} data-item={item.name}
-                       onMouseDown={this.onMouseDown}
-                       onClick={this.onMenuItemClick}
-                    >{item.title}</a>
+                    <a key={item.name} data-menu={menu.name} data-item={item.name}>{item.title}</a>
                 )}
             </div>
         </div>);
