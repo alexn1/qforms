643e1d24a9a1754cda0a03ebc100ca5a7b77be9d
Alexander Nesterenko <alexn140@gmail.com>
2020-10-07 09:08:06 +0700

    DropdownButton.onBlur

--- src/frontend/common/jsx/DropdownButton/DropdownButton.jsx
+++ src/frontend/common/jsx/DropdownButton/DropdownButton.jsx
@@ -7,15 +7,21 @@ class DropdownButton extends React.Component{
         };
     }
     onClick = e => {
-        console.log('DropdownButton.onClick', this.state);
+        console.log('DropdownButton.onClick');
         this.setState(state => ({
             open: !state.open
         }));
     }
+    onBlur = e => {
+        console.log('DropdownButton.onBlur');
+        if (this.state.open) {
+            this.setState({open: false});
+        }
+    }
     render() {
         return (
             <div className={`DropdownButton ${this.state.open && 'show'}`}>
-                <button onClick={this.onClick}>Actions</button>
+                <button onClick={this.onClick} onBlur={this.onBlur}>Actions</button>
                 <ul>
                     {this.props.actions.map(action => <li key={action.name} data-action={action.name}>{action.title}</li>)}
                 </ul>
