76c58476101f1c99d0f86d9da914ceaba0a1b93c
Alexander Nesterenko <alexn140@gmail.com>
2020-10-08 21:21:01 +0700

    ref

--- src/frontend/common/jsx/FormGrid/FormGrid.jsx
+++ src/frontend/common/jsx/FormGrid/FormGrid.jsx
@@ -2,6 +2,36 @@ class FormGrid extends ReactComponent {
     getFieldId(field) {
         return `${field.getForm().getPage().id}_${field.getForm().getName()}_${field.getName()}`;
     }
+    getFieldLabel(field) {
+        return (
+            <div key={`label.${field.getName()}`} className={`label ${field.getName()}`}>
+                {field.data.caption}:
+                {field.data.notNull === 'true' && <span style={{color: 'red'}}>*</span>}
+            </div>
+        );
+    }
+    getFieldView(field) {
+        return (
+            <div
+                key={`field.${field.getName()}`}
+                id={this.getFieldId(field)}
+                className={`field ${field.getName()}`}>
+                {this.getFieldViewContent(field)}
+            </div>
+        );
+    }
+    getFieldViewContent(field) {
+        return (
+            <TextBox></TextBox>
+        );
+    }
+    getFieldTooltip(field) {
+        return (
+            <div key={`tooltip.${field.getName()}`} className={`tooltip ${field.getName()}`}>
+                <Tooltip position="left" type="alert" />
+            </div>
+        );
+    }
     render() {
         const ctrl = this.props.ctrl;
         const model = ctrl.model;
@@ -11,17 +41,11 @@ class FormGrid extends ReactComponent {
                 {Object.keys(model.fields).map(name => {
                     const field = model.fields[name];
                     // console.log('field:', field);
-                    return ([
-                        <div key={`label.${field.getName()}`} className={`label ${field.getName()}`}>
-                            {field.data.caption}:
-                            {field.data.notNull === 'true' && <span style={{color: 'red'}}>*</span>}
-                        </div>,
-                        <div
-                            key={`field.${field.getName()}`}
-                            id={this.getFieldId(field)}
-                            className={`field ${field.getName()}`}></div>,
-                        <div key={`tooltip.${field.getName()}`} className={`tooltip ${field.getName()}`}></div>
-                    ]);
+                    return [
+                        this.getFieldLabel(field),
+                        this.getFieldView(field),
+                        this.getFieldTooltip(field)
+                    ];
                 })}
             </div>
         );
