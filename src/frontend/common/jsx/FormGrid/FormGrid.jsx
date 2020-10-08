7571596bd57c2a781dd1410170ab337d909ed46c
Alexander Nesterenko <alexn140@gmail.com>
2020-10-08 20:35:59 +0700

    fields

--- src/frontend/common/jsx/FormGrid/FormGrid.jsx
+++ src/frontend/common/jsx/FormGrid/FormGrid.jsx
@@ -1,12 +1,27 @@
 class FormGrid extends ReactComponent {
+    getFieldId(field) {
+        return `${field.getForm().getPage().id}_${field.getForm().getName()}_${field.getName()}`;
+    }
     render() {
         const ctrl = this.props.ctrl;
+        const model = ctrl.model;
+        // console.log('model:', model);
         return (
             <div className="FormGrid">
-                {Object.keys(ctrl.model.fields).map(name => {
-                    const field = ctrl.model.fields[name];
-                    console.log('field:', field);
-                    return (<div key={name} className={`label ${name}`}>{field.data.caption}:</div>);
+                {Object.keys(model.fields).map(name => {
+                    const field = model.fields[name];
+                    // console.log('field:', field);
+                    return ([
+                        <div key={`label.${field.getName()}`} className={`label ${field.getName()}`}>
+                            {field.data.caption}:
+                            {field.data.notNull === 'true' && <span style={{color: 'red'}}>*</span>}
+                        </div>,
+                        <div
+                            key={`field.${field.getName()}`}
+                            id={this.getFieldId(field)}
+                            className={`field ${field.getName()}`}></div>,
+                        <div key={`tooltip.${field.getName()}`} className={`tooltip ${field.getName()}`}></div>
+                    ]);
                 })}
             </div>
         );
