e3b8a18d1b5c6a223ab885febbac295fd54053b3
Alexander Nesterenko <alexn140@gmail.com>
2020-10-12 01:10:22 +0700

    grid layout

--- src/frontend/common/jsx/Grid/Grid.jsx
+++ src/frontend/common/jsx/Grid/Grid.jsx
@@ -6,20 +6,80 @@ class Grid extends ReactComponent {
                     <table>
                         <tbody>
                             <tr>
-                                <td>
+                                <td style={{width: '100px'}}>
                                     <div>id</div>
                                     <span className="resize"></span>
                                 </td>
-                                <td>
+                                <td style={{width: '100px'}}>
                                     <div>created</div>
                                     <span className="resize"></span>
                                 </td>
+                                <td></td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
                 <div className="block"></div>
-                <div className="body"></div>
+                <div className="body">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td style={{width: '100px'}}>
+                                    <div>1</div>
+                                </td>
+                                <td style={{width: '100px'}}>
+                                    <div>2</div>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <tr className="active">
+                                <td className="active">
+                                    <div>1</div>
+                                </td>
+                                <td>
+                                    <div>2</div>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div>1</div>
+                                </td>
+                                <td>
+                                    <div>2</div>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div>1</div>
+                                </td>
+                                <td>
+                                    <div>2</div>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div>1</div>
+                                </td>
+                                <td>
+                                    <div>2</div>
+                                </td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div>1</div>
+                                </td>
+                                <td>
+                                    <div>2</div>
+                                </td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         );
     }
