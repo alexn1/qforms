db800eac52315fad0cdaf5c5ca251eb6297bc5ac
Alexander Nesterenko <alexn140@gmail.com>
2020-10-28 15:30:00 +0700

    SdiApplicationController.createPage

--- src/frontend/viewer/class/Controller/ApplicationController/SdiApplicationController/SdiApplicationController.js
+++ src/frontend/viewer/class/Controller/ApplicationController/SdiApplicationController/SdiApplicationController.js
@@ -3,6 +3,26 @@ class SdiApplicationController extends ApplicationController {
     getViewClass() {
         return SdiApplicationView;
     }
+    init() {
+        super.init();
+        this.activePage = this.createPage();
+    }
+    createPage() {
+        const name = Object.keys(this.model.data.pages).length ? Object.keys(this.model.data.pages)[0] : null;
+        if (!name) return null;
+
+        // model
+        const page = new Page(this.model.data.pages[name], this.model, {
+            id   : `p${this.getNextPageId()}`,
+            modal: false
+        });
+        page.init();
+
+        // controller
+        const pc = PageController.create(page, this);
+        pc.init();
+        return pc;
+    }
     findPageControllerByPageNameAndKey(pageName, key) {
         if (this.activePage && this.activePage.model.getName() === pageName && this.activePage.model.getKey() === key) {
             return this.activePage;
