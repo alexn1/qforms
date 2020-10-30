f586168c2ada16c6d2afa35982f160a5fa3de1ee
Alexander Nesterenko <alexn140@gmail.com>
2020-10-30 14:29:27 +0700

    save imges as text

--- src/frontend/viewer/class/Controller/FieldController/RowFormFieldController/RowFormImageFieldController/RowFormImageFieldController.js
+++ src/frontend/viewer/class/Controller/FieldController/RowFormFieldController/RowFormImageFieldController/RowFormImageFieldController.js
@@ -3,9 +3,4 @@ class RowFormImageFieldController extends RowFormFieldController {
     getViewClass() {
         return RowFormImageFieldView;
     }
-    renderValueForView() {
-        // console.log('RowFormImageFieldController.renderValueForView', this.state.value);
-        const buffer = Helper.base64ToArrayBuffer(this.state.value);
-        return Helper.createObjectUrl(buffer);
-    }
 }
