05808419c6a3fe412138a272024bf0cf2996fe1c
Alexander Nesterenko <alexn140@gmail.com>
2020-10-30 23:38:10 +0700

    ref

--- src/frontend/viewer/class/Controller/FieldController/TableFormFieldController/TableFormDatePickerFieldController/TableFormDatePickerFieldController.js
+++ src/frontend/viewer/class/Controller/FieldController/TableFormFieldController/TableFormDatePickerFieldController/TableFormDatePickerFieldController.js
@@ -6,7 +6,7 @@ class TableFormDatePickerFieldController extends TableFormFieldController {
     }
     getValueForView(row) {
         const value = this.model.getValue(row);
-        if (value) return Helper.formatDate(this.model.getValue(row), this.model.getFormat() || '{DD}.{MM}.{YYYY} {hh}:{mm}:{ss}');
+        if (value) return Helper.formatDate(value, this.model.getFormat() || '{DD}.{MM}.{YYYY} {hh}:{mm}:{ss}');
         return '';
     }
 }
