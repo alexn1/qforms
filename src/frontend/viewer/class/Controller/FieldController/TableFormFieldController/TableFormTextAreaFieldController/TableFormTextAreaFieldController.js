b00dc38dbd8c4c9e8e8607d572f7630cfd747d83
Alexander Nesterenko <alexn140@gmail.com>
2020-11-01 03:49:50 +0700

    removed not used table form controllers

--- src/frontend/viewer/class/Controller/FieldController/TableFormFieldController/TableFormTextAreaFieldController/TableFormTextAreaFieldController.js
+++ src/frontend/viewer/class/Controller/FieldController/TableFormFieldController/TableFormTextAreaFieldController/TableFormTextAreaFieldController.js
@@ -2,18 +2,4 @@
 
 class TableFormTextAreaFieldController extends TableFormFieldController {
 
-    /*setPlaceHolder(view, value) {
-        // console.log('TableFormTextAreaFieldController.setPlaceHolder', this.model.getFullName(), value);
-        if (this.model.getForm().getClassName() === 'RowForm') {
-            if (ApplicationController.isInDebugMode()) {
-                if (value === undefined) {
-                    view.firstElementChild.placeholder = 'undefined';
-                } else if (value === null) {
-                    view.firstElementChild.placeholder = 'null';
-                } else if (value === '') {
-                    view.firstElementChild.placeholder = 'empty string';
-                }
-            }
-        }
-    }*/
 }
