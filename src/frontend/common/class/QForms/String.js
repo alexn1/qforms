549d9011727d9127fa093421b1ade09589439d79
Alexander Nesterenko <alexn140@gmail.com>
2020-11-01 22:19:39 +0700

    misc

--- src/frontend/common/class/QForms/String.js
+++ src/frontend/common/class/QForms/String.js
@@ -1,7 +1,9 @@
 'use strict';
 
+/*
 String.prototype.template = function (values) {
     return this.replace(/\{([\w]+)\}/g, (text, name) => {
         return values.hasOwnProperty(name) ? values[name] : text;
     });
 };
+*/
