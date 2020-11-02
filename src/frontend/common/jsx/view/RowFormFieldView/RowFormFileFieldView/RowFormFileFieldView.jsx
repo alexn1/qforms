2ec403d6ee8e593c08d6e6c319c2942af8a77e2e
Alexander Nesterenko <alexn140@gmail.com>
2020-11-02 22:43:58 +0700

    updateSize

--- src/frontend/common/jsx/view/RowFormFieldView/RowFormFileFieldView/RowFormFileFieldView.jsx
+++ src/frontend/common/jsx/view/RowFormFieldView/RowFormFileFieldView/RowFormFileFieldView.jsx
@@ -2,9 +2,17 @@ class RowFormFileFieldView extends RowFormFieldView {
     constructor(props) {
         super(props);
         this.image = React.createRef();
-        this.state = {
-            size: null
-        };
+        this.div   = React.createRef();
+    }
+    getImage() {
+        return this.image.current;
+    }
+    getDiv() {
+        return this.div.current;
+    }
+    updateSize() {
+        const ns = this.getImage().getNaturalSize();
+        this.getDiv().innerText = `${ns[0]}×${ns[1]}`;
     }
     render() {
         const ctrl = this.props.ctrl;
@@ -15,17 +23,20 @@ class RowFormFileFieldView extends RowFormFieldView {
                     <a href={value} target="_blank">
                         <Image ref={this.image} src={value}/>
                     </a>
-                    <div>{this.state.size ? JSON.stringify([...this.state.size, ...([value.length])]) : null}</div>
+                    <span className="size" ref={this.div}></span>
+                    <span className="length">{value.length}</span>
                 </div>
             }
-            {/*{!!value && <a href={value} target="_blank">Download</a>}*/}
+            {!!value && <a href={value} target="_blank">Download</a>}
             <input type="file" onChange={ctrl.onFileChange} disabled={!ctrl.isEditable()}/>
         </div>;
     }
     componentDidMount() {
-        setTimeout(()=> {
-            // console.log('this.image.current.getNaturalSize()', this.image.current.getNaturalSize());
-            this.setState({size: this.image.current.getNaturalSize()});
-        }, 0);
+        // console.log('RowFormFileFieldView.componentDidMount', this.props.ctrl.model.getFullName());
+        setTimeout(() => this.updateSize(), 0);
+    }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        // console.log('RowFormFileFieldView.componentDidUpdate', this.props.ctrl.model.getFullName(), snapshot);
+        setTimeout(() => this.updateSize(), 0);
     }
 }
