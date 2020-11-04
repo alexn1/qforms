2b38b71fc49c1975e2eb8af8e1dfebcb67770919
Alexander Nesterenko <alexn140@gmail.com>
2020-11-05 00:55:26 +0700

    Button ref

--- src/frontend/common/jsx/widget/Button/Button.jsx
+++ src/frontend/common/jsx/widget/Button/Button.jsx
@@ -6,60 +6,25 @@ class Button extends ReactComponent {
     }
     isDisabled() {
         if (this.props.enabled !== undefined) return !this.props.enabled;
-        // if (this.props.isDisabled) return this.props.isDisabled(this.props.name);
         return this.state.disabled;
     }
-    /*shouldComponentUpdate(nextProps, nextState) {
-        if (this.props.enabled !== undefined) {
-            return this.props.enabled !== nextProps.enabled;
-        }
-        return this.state.disabled !== nextState.disabled;
-    }*/
-    /*disable() {
-        // console.log('Button.disable');
-        return new Promise(resolve => this.setState({disabled: true}, resolve));
-    }*/
-    /*enable() {
-        // console.log('Button.enable');
-        return new Promise(resolve => this.setState({disabled: false}, resolve));
-    }*/
-    /*setDisabled(value) {
-        if (typeof value !== 'boolean') throw new Error('value must be boolean');
-        return new Promise(resolve => {
-            if (this.state.disabled === value) {
-                resolve();
-            } else {
-                this.setState({disabled: value}, resolve)
-            }
-        });
-    }*/
-    /*setEnabled(value) {
-        if (typeof value !== 'boolean') throw new Error('value must be boolean');
-        return new Promise(resolve => {
-            if (this.state.disabled === !value) {
-                resolve();
-            } else {
-                this.setState({disabled: !value}, resolve)
-            }
-        });
-    }*/
     onClick = e => {
         // console.log('Button.onClick', e);
         if (this.props.onClick) this.props.onClick(e);
     }
     isVisible() {
-        return this.props.visible;
+        return this.props.visible === undefined ? true : this.props.visible;
     }
     render() {
         // console.log('Button.render', this.props.title, this.props);
         return (
-            <button
+            <button className="Button"
                 name={this.props.name}
                 disabled={this.isDisabled()}
                 onClick={this.onClick}
                 style={{
-                    display: this.isVisible() === false ? 'none' : 'inline-block',
-                    width: this.props.width
+                    display: !this.isVisible() ? 'none' : null,
+                    width  : this.props.width
                 }}
             >{this.props.title || this.props.children}</button>
         );
