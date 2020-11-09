8c6632bc0c2baa9fa1e30ea415cbfb14c5eaf37d
Alexander Nesterenko <alexn140@gmail.com>
2020-11-09 21:16:45 +0700

    ref

--- src/frontend/common/class/QForms/String.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/*
-String.prototype.template = function (values) {
-    return this.replace(/\{([\w]+)\}/g, (text, name) => {
-        return values.hasOwnProperty(name) ? values[name] : text;
-    });
-};
-*/
