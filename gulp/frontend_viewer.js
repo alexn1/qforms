6780523873c19bb22960d3d0876ec7a805ade9e7
Alexander Nesterenko <alexn140@gmail.com>
2020-11-22 20:45:16 +0700

    viewer source map

--- gulp/frontend_viewer.js
+++ gulp/frontend_viewer.js
@@ -5,29 +5,36 @@ const path      = require('path');
 const concat    = require('gulp-concat');
 const less      = require('gulp-less');
 const babel      = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
 
 const SRC_PATH   = "./src";
 const BUILD_PATH = './build';
 
 function frontend_viewer_class_js() {
     return gulp.src(path.join(SRC_PATH, 'frontend/viewer/class/**/*.js'))
+        .pipe(sourcemaps.init())
         .pipe(concat('viewer.js'))
         //.pipe(uglify())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/js')));
 }
 
 function frontend_viewer_jsx_js() {
     return gulp.src(path.join(SRC_PATH, 'frontend/viewer/class/View/**/*.jsx'))
+        .pipe(sourcemaps.init())
         .pipe(babel())
         .pipe(concat('viewer-jsx.js'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/js')));
 }
 
 function frontend_viewer_jsx_less() {
     return gulp.src(path.join(SRC_PATH, 'frontend/viewer/class/View/**/*.less'))
+        .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(concat('viewer-jsx.css'))
         // .pipe(minifyCss())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/css')));
 }
 
