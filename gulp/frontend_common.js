381861f6de35a3e7c3a72f7d2ef55a014f4734a1
Alexander Nesterenko <alexn140@gmail.com>
2020-11-22 20:48:08 +0700

    common source map

--- gulp/frontend_common.js
+++ gulp/frontend_common.js
@@ -5,29 +5,36 @@ const minifyCss = require('gulp-minify-css');
 const concat    = require('gulp-concat');
 const less      = require('gulp-less');
 const babel      = require('gulp-babel');
+const sourcemaps = require('gulp-sourcemaps');
 
 const BUILD_PATH = './build';
 const SRC_PATH   = "./src";
 
 function frontend_common_jsx_less() {
     return gulp.src(path.join(SRC_PATH, 'frontend/common/jsx/**/*.less'))
+        .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(concat('common-jsx.css'))
         // .pipe(minifyCss())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/css')));
 }
 
 function frontend_common_jsx_js() {
     return gulp.src(path.join(SRC_PATH, 'frontend/common/jsx/**/*.jsx'))
+        .pipe(sourcemaps.init())
         .pipe(babel())
         .pipe(concat('common-jsx.js'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/js')));
 }
 
 function frontend_common_class_js() {
     return gulp.src(path.join(SRC_PATH, 'frontend/common/class/**/*.js'))
+        .pipe(sourcemaps.init())
         .pipe(concat('common.js'))
         //.pipe(uglify())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/js')));
 }
 
