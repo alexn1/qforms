32f1742caf346b16dc1c31fc89826db6326e27fc
Alexander Nesterenko <alexn140@gmail.com>
2020-12-06 02:11:40 +0700

    TimeBox value

--- src/frontend/common/jsx/widget/TimeBox/TimeBox.jsx
+++ src/frontend/common/jsx/widget/TimeBox/TimeBox.jsx
@@ -1,13 +1,20 @@
-class TimeBox extends TextBox {
+class TimeBox extends ReactComponent {
+    constructor(props) {
+        // console.log('TimeBox.constructor', props);
+        super(props);
+        this.state = {
+            value: props.value || null,
+        };
+        if (props.value && typeof props.value !== 'number') {
+            throw new Error(`need number type, got ${typeof props.value}`);
+        }
+    }
     onKeyPress = event => {
         // console.log('TimeBox.onKeyPress', event.key, event.target.value);
         if (!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(event.key)) {
             console.log('cancel', event.key);
             event.preventDefault();
         }
-        /*if (event.target.value.length + 1 > 5) {
-            event.preventDefault();
-        }*/
     }
     formatValue(value) {
         let min = '';
@@ -47,21 +54,24 @@ class TimeBox extends TextBox {
         }
 
         const inEnd = start === end && start === target.value.length;
-        const value = this.formatValue(target.value);
-        console.log('value:', value);
+        const stringValue = this.formatValue(target.value);
+        console.log('value:', stringValue);
 
         // console.log('before:', target.selectionStart, target.selectionEnd);
-        this.setState({value}, () => {
+        this.setState({value: stringValue}, () => {
             // console.log('after:', target.selectionStart, target.selectionEnd);
             // console.log('inEnd:', inEnd);
             if (!inEnd) {
                 target.selectionStart = start;
-                target.selectionEnd = end;
+                target.selectionEnd   = end;
             }
         });
-        //this.setState({value: e.target.value});
+
+        /*if (this.props.onChange) {
+            this.props.onChange(value);
+        }*/
     }
-    onKeyDown = event => {
+    /*onKeyDown = event => {
         console.log('TimeBox.onKeyDown', event.which, event.target.value.length, event.target.selectionStart, event.target.selectionEnd, event.key);
         const mask = '00:00';
         if ([8, 46, 37, 39, 36, 35].includes(event.which)) return;
@@ -75,48 +85,19 @@ class TimeBox extends TextBox {
             event.stopPropagation();
             event.preventDefault();
         }
-
-        /*event.target.value = '12:'
-        event.target.selectionStart = 0;
-        event.target.selectionEnd = 0;*/
-        /*
-        var mask = '99:99';
-        with (event) {
-        if (!event.charCode) return
-        var c = String.fromCharCode(event.charCode)
-        if (c.match(/\D/)) return
-        // with (event.target) {
-        var val = event.target.value.substring(0, event.target.selectionStart) + c + event.target.value.substr(event.target.selectionEnd)
-        var pos = event.target.selectionStart + 1
-        // }
-        //}
-        var nan = count(val, /\D/, pos) // nan va calcolato prima di eliminare i separatori
-        val = val.replace(/\D/g,'')
-
-        var mask = mask.match(/^(\D*)(.+9)(\D*)$/)
-        if (!mask) return // meglio exception?
-        if (val.length > count(mask[2], /9/)) return
-
-        for (var txt='', im=0, iv=0; im<mask[2].length && iv<val.length; im+=1) {
-            var c = mask[2].charAt(im)
-            txt += c.match(/\D/) ? c : val.charAt(iv++)
-        }
-
-        // with (event.target) {
-        event.target.value = mask[1] + txt + mask[3]
-        event.target.selectionStart = event.target.selectionEnd = pos + (pos==1 ? mask[1].length : count(event.target.value, /\D/, pos) - nan)
-        // }
-
-        function count(str, c, e) {
-            e = e || str.length
-            for (var n=0, i=0; i<e; i+=1) if (str.charAt(i).match(c)) n+=1
-            return n
-        }*/
-    }
-    onKeyUp = event => {
+    }*/
+    /*onKeyUp = event => {
         console.log('TimeBox.onKeyUp', event.which, event.target.value.length, event.target.selectionStart, event.target.selectionEnd, event.target.value);
         event.stopPropagation();
         event.preventDefault();
+    }*/
+    getStringValue() {
+        return this.state.value.toString();
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        // console.log('TimeBox.shouldComponentUpdate', 'nextProps:', nextProps, 'nextState:', nextState);
+        this.state.value = nextProps.value;
+        return true;
     }
     render() {
         // console.log('TimeBox.render');
@@ -126,7 +107,7 @@ class TimeBox extends TextBox {
                 id={this.props.id}
                 className={this.getClassName()}
                 type="text"
-                value={this.state.value}
+                value={this.getStringValue()}
                 readOnly={this.props.readOnly}
                 onChange={this.onChange}
                 placeholder={this.props.placeholder}
