8e732a819e7afc3fce125252b297174f7bb1ec11
Alexander Nesterenko <alexn140@gmail.com>
2020-12-07 13:53:01 +0700

    TableFormTimeFieldController.getValueForView

--- src/frontend/viewer/class/Controller/FieldController/TableFormFieldController/TableFormTimeFieldController/TableFormTimeFieldController.js
+++ src/frontend/viewer/class/Controller/FieldController/TableFormFieldController/TableFormTimeFieldController/TableFormTimeFieldController.js
@@ -2,4 +2,8 @@ class TableFormTimeFieldController extends TableFormFieldController {
     getViewClass() {
         return TableFormTextBoxFieldView;
     }
+    getValueForView(row) {
+        const value = this.model.getValue(row);
+        return TimeBox.getStringValue(value);
+    }
 }
