97dd96eae14cc21aa7fd0dfae0f8b0eb568a431b
Alexander Nesterenko <alexn140@gmail.com>
2020-12-10 16:05:24 +0700

    field layout

--- src/frontend/viewer/class/View/RowFormFieldView/RowFormTimeFieldView/RowFormTimeFieldView.jsx
+++ src/frontend/viewer/class/View/RowFormFieldView/RowFormTimeFieldView/RowFormTimeFieldView.jsx
@@ -1,4 +1,7 @@
 class RowFormTimeFieldView extends RowFormFieldView {
+    onCloseClick = async e => {
+
+    }
     render() {
         const ctrl = this.props.ctrl;
         return <div className={this.getClassName()}>
@@ -10,6 +13,14 @@ class RowFormTimeFieldView extends RowFormFieldView {
                 onBlur={ctrl.onBlur}
                 placeholder={ctrl.getPlaceholder()}
             />
+            <div className={`close visible`} onClick={this.onCloseClick}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">
+                    <line x1="2" y1="2" x2="8" y2="8" stroke="#aaa" strokeWidth="1.1" strokeLinecap="round"
+                          strokeMiterlimit="10"></line>
+                    <line x1="8" y1="2" x2="2" y2="8" stroke="#aaa" strokeWidth="1.1" strokeLinecap="round"
+                          strokeMiterlimit="10"></line>
+                </svg>
+            </div>
         </div>
     }
 }
