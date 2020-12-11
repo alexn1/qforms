5576045a26d915e8e74fb715378698f70366a6ca
Alexander Nesterenko <alexn140@gmail.com>
2020-12-11 13:44:14 +0700

    getDefaultValue

--- src/frontend/viewer/class/Controller/FieldController/RowFormFieldController/RowFormTimeFieldController/RowFormTimeFieldController.js
+++ src/frontend/viewer/class/Controller/FieldController/RowFormFieldController/RowFormTimeFieldController/RowFormTimeFieldController.js
@@ -1,4 +1,8 @@
 class RowFormTimeFieldController extends RowFormFieldController {
+    constructor(...args) {
+        super(...args);
+        this.defaultStringValue = null;
+    }
     getViewClass() {
         return RowFormTimeFieldView;
     }
@@ -8,5 +12,8 @@ class RowFormTimeFieldController extends RowFormFieldController {
     setValueFromView(viewValue) {
         if (isNaN(viewValue)) throw new Error('wrong time');
         this.setValue(viewValue);
+    }
+    getDefaultValue() {
+        return this.defaultStringValue ? TimeBox.getIntegerValue(this.defaultStringValue) : null;
     }
 }
