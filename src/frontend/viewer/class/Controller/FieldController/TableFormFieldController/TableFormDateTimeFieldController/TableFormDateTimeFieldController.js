fdff589f2ecbc53a179036f31b1152ec30e42e70
Alexander Nesterenko <alexn140@gmail.com>
2020-12-17 01:52:24 +0700

    use TableFormDatePickerFieldView

--- src/frontend/viewer/class/Controller/FieldController/TableFormFieldController/TableFormDateTimeFieldController/TableFormDateTimeFieldController.js
+++ src/frontend/viewer/class/Controller/FieldController/TableFormFieldController/TableFormDateTimeFieldController/TableFormDateTimeFieldController.js
@@ -1,3 +1,10 @@
 class TableFormDateTimeFieldController extends TableFormFieldController {
-
+    getViewClass() {
+        return TableFormDatePickerFieldView;
+    }
+    getValueForView(row) {
+        const value = this.model.getValue(row);
+        if (value) return Helper.formatDate(value, this.model.getFormat() || '{DD}.{MM}.{YYYY} {hh}:{mm}:{ss}');
+        return '';
+    }
 }
