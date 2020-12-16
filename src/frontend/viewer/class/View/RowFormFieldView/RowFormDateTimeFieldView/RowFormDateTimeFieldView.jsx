fb8b983fb36e1b42f3503db86206708a47bb8021
Alexander Nesterenko <alexn140@gmail.com>
2020-12-16 23:20:31 +0700

    TimeBox close button

--- src/frontend/viewer/class/View/RowFormFieldView/RowFormDateTimeFieldView/RowFormDateTimeFieldView.jsx
+++ src/frontend/viewer/class/View/RowFormFieldView/RowFormDateTimeFieldView/RowFormDateTimeFieldView.jsx
@@ -1,4 +1,7 @@
 class RowFormDateTimeFieldView extends RowFormFieldView {
+    isCloseVisible() {
+        return true;
+    }
     render() {
         const ctrl = this.props.ctrl;
         return <div className={this.getClassName()}>
@@ -14,6 +17,14 @@ class RowFormDateTimeFieldView extends RowFormFieldView {
             />
             <div>
                 <TimeBox placeholder="00:00"/>
+                <div className={`close ${this.isCloseVisible() ? 'visible' : ''}`} onClick={this.onCloseClick}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10">
+                        <line x1="2" y1="2" x2="8" y2="8" stroke="#aaa" strokeWidth="1.1" strokeLinecap="round"
+                              strokeMiterlimit="10"></line>
+                        <line x1="8" y1="2" x2="2" y2="8" stroke="#aaa" strokeWidth="1.1" strokeLinecap="round"
+                              strokeMiterlimit="10"></line>
+                    </svg>
+                </div>
             </div>
         </div>;
     }
