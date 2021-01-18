90fddafb3235af2eef6122f8d0149c70d14f70a2
Alexander Nesterenko <alexn140@gmail.com>
2021-01-18 15:09:20 +0700

    compile js for editor

--- gulp/frontend_editor.js
+++ gulp/frontend_editor.js
@@ -5,6 +5,7 @@ const gulp      = require('gulp');
 const concat    = require('gulp-concat');
 const less      = require('gulp-less');
 const sourcemaps = require('gulp-sourcemaps');
+const babel      = require('gulp-babel');
 
 const BUILD_PATH = './build';
 const SRC_PATH   = "./src";
@@ -38,6 +39,25 @@ function frontend_editor_class_js() {
         .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/js')));
 }
 
+function frontend_editor_jsx_js() {
+    return gulp.src(path.join(SRC_PATH, 'frontend/editor/class/**/*.jsx'))
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(concat('editor-jsx.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/js')));
+}
+
+function frontend_editor_jsx_less() {
+    return gulp.src(path.join(SRC_PATH, 'frontend/editor/class/**/*.less'))
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(concat('editor-jsx.css'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/css')));
+}
+
+
 function frontend_editor_lib() {
     return gulp.src(path.join(SRC_PATH, 'frontend/editor/lib/**/*'))
         .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/lib')));
@@ -48,7 +68,14 @@ function frontend_editor_img() {
         .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/img')));
 }
 
-const frontend_editor_class = gulp.series(frontend_editor_class_js, frontend_editor_class_css, frontend_editor_class_html, frontend_editor_class_ejs);
+const frontend_editor_class = gulp.series(
+    frontend_editor_class_js,
+    frontend_editor_class_css,
+    frontend_editor_class_html,
+    frontend_editor_class_ejs,
+    frontend_editor_jsx_js,
+    frontend_editor_jsx_less,
+);
 const frontend_editor = gulp.series(frontend_editor_class, frontend_editor_lib, frontend_editor_img);
 
 module.exports = frontend_editor;
