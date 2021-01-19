8dc8e4fcb81656898035d20c292e0cfd005f43fa
Alexander Nesterenko <alexn140@gmail.com>
2021-01-19 15:32:42 +0700

    render options

--- src/frontend/editor/class/PropertyGrid2/PropertyGrid2.jsx
+++ src/frontend/editor/class/PropertyGrid2/PropertyGrid2.jsx
@@ -1,14 +1,24 @@
 class PropertyGrid2 extends ReactComponent {
+    renderInput(name) {
+        const obj = this.props.obj;
+        return <input name={name} value={obj[name]} spellCheck="false"/>;
+    }
+    renderSelect(name) {
+        const obj = this.props.obj;
+        const options = this.props.options;
+        return <select name={name} value={obj[name]}>
+            {options[name].map(value => <option key={value}>{value}</option>)}
+        </select>;
+    }
     renderRows() {
-        const  obj = this.props.obj;
-        return Object.keys(obj).map(name => {
-            return <tr key={name}>
-                <td>{name}</td>
-                <td>
-                    <input name={name} spellCheck="false" value={obj[name]}/>
-                </td>
-            </tr>;
-        });
+        const obj     = this.props.obj;
+        const options = this.props.options;
+        return Object.keys(obj).map(name => <tr key={name}>
+            <td>{name}</td>
+            <td>
+                {options && options[name] !== undefined ? this.renderSelect(name) : this.renderInput(name)}
+            </td>
+        </tr>);
     }
     render() {
         return <div className={'PropertyGrid2'}>
