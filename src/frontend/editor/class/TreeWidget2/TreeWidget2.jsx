137319ffa4f071b19341fedadd73d763e99d8978
Alexander Nesterenko <alexn140@gmail.com>
2021-01-21 16:38:21 +0700

    sample tree

--- src/frontend/editor/class/TreeWidget2/TreeWidget2.jsx
+++ src/frontend/editor/class/TreeWidget2/TreeWidget2.jsx
@@ -2,7 +2,22 @@ class TreeWidget2 extends ReactComponent {
     render() {
         return <div className={'TreeWidget2'}>
             <ul>
-                <li></li>
+                <li className={'opened'}>
+                    <div style={{paddingLeft: 5}}>
+                        <span className={'node'}></span>
+                        &nbsp;
+                        <span>admin</span>
+                    </div>
+                    <ul>
+                        <li className={'opened'}>
+                            <div style={{paddingLeft: 20}}>
+                                <span className={'node'}></span>
+                                &nbsp;
+                                <span>Databases</span>
+                            </div>
+                        </li>
+                    </ul>
+                </li>
             </ul>
         </div>;
     }
