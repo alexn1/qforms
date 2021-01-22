c1203048a576df2e9e4e4762be2a4a440dada61a
Alexander Nesterenko <alexn140@gmail.com>
2021-01-22 16:54:53 +0700

    selectedItem

--- src/frontend/editor/class/TreeWidget2/TreeItem2.jsx
+++ src/frontend/editor/class/TreeWidget2/TreeItem2.jsx
@@ -8,6 +8,7 @@ class TreeItem2 extends ReactComponent {
     }
     render() {
         const item = this.props.item;
+        const tree = this.props.tree;
         return <li key={item.title} className={'opened'}>
             <div onMouseDown={this.onDivMouseDown}>
                 <span className={'node'} onMouseDown={this.onNodeMouseDown}/>
@@ -16,7 +17,7 @@ class TreeItem2 extends ReactComponent {
             </div>
             {item.items &&
             <ul style={{paddingLeft: 15}}>
-                {item.items.map(item => <TreeItem2 key={item.title} item={item}/>)}
+                {item.items.map(item => <TreeItem2 key={item.title} tree={tree} item={item}/>)}
             </ul>
             }
         </li>;
--- src/frontend/editor/class/TreeWidget2/TreeWidget2.jsx
+++ src/frontend/editor/class/TreeWidget2/TreeWidget2.jsx
@@ -1,10 +1,16 @@
 class TreeWidget2 extends ReactComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedItem: null
+        };
+    }
     render() {
         // console.log('TreeWidget2.render', this.props.items);
         const items = this.props.items;
         return <div className={'TreeWidget2'}>
             <ul style={{paddingLeft: 5}}>
-                {items.map(item => <TreeItem2 key={item.title} item={item}/>)}
+                {items.map(item => <TreeItem2 key={item.title} tree={this} item={item}/>)}
             </ul>
         </div>;
     }
