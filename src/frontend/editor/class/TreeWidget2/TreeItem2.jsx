320020b1ba1115d7949daf5e4ff8525617f7ce58
Alexander Nesterenko <alexn140@gmail.com>
2021-01-23 00:19:28 +0700

    open/close

--- src/frontend/editor/class/TreeWidget2/TreeItem2.jsx
+++ src/frontend/editor/class/TreeWidget2/TreeItem2.jsx
@@ -1,4 +1,10 @@
 class TreeItem2 extends ReactComponent {
+    constructor(props) {
+        super(props);
+        this.state = {
+            opened: false
+        };
+    }
     onDivMouseDown = e => {
         console.log('TreeItem2.onDivMouseDown', e.currentTarget);
         this.props.tree.select(this.props.item);
@@ -6,16 +12,25 @@ class TreeItem2 extends ReactComponent {
     onNodeMouseDown = e => {
         console.log('TreeItem2.onNodeMouseDown', e.currentTarget);
         e.stopPropagation();
+        this.setState(prevState => {
+            return {opened: !prevState.opened};
+        });
     }
     isSelected() {
         return this.props.tree.state.selectedItem === this.props.item;
     }
+    isNode() {
+        return !!this.props.item.items;
+    }
+    isOpened() {
+        return this.state.opened;
+    }
     render() {
         const item = this.props.item;
         const tree = this.props.tree;
-        return <li key={item.title} className={'opened'}>
+        return <li key={item.title} className={this.isOpened() ? 'opened' : null}>
             <div onMouseDown={this.onDivMouseDown} className={this.isSelected() ? 'active' : null} style={{paddingLeft: this.props.paddingLeft}}>
-                <span className={'node'} onMouseDown={this.onNodeMouseDown}/>
+                <span className={this.isNode() ? 'node' : 'leaf'} onMouseDown={this.onNodeMouseDown}/>
                 &nbsp;
                 <span>{item.title}</span>
             </div>
