4b331bb9dcb03d5a0c142449a8670d590ba71b17
Alexander Nesterenko <alexn140@gmail.com>
2021-02-10 11:52:29 +0700

    ref

--- src/frontend/editor/class/EditorController/EditorView.jsx
+++ src/frontend/editor/class/EditorController/EditorView.jsx
@@ -1,27 +1,27 @@
 class EditorView extends ReactComponent {
     render() {
         const ctrl = this.props.ctrl;
-        return <Tab
-            classList={['Tab-blue', 'full']}
-            tabs={[
-                {
-                    name   : 'properties',
-                    title  : 'Properties',
-                    content: <div style={{height: '100%'}}>
-                        <TreeWidget2
-                            onCreate={c => ctrl.treeWidget2 = c}
-                            items={ctrl.items}
-                            onItemSelect={ctrl.onItemSelect2}
-                            onItemDoubleClick={ctrl.onItemDoubleClick2}
-                            onItemOpen={ctrl.onItemOpen2}
-                        />
-                        <PropertyGrid2
+        return <div style={{height: '100%'}}>
+            <TreeWidget2
+                onCreate={c => ctrl.treeWidget2 = c}
+                items={ctrl.items}
+                onItemSelect={ctrl.onItemSelect2}
+                onItemDoubleClick={ctrl.onItemDoubleClick2}
+                onItemOpen={ctrl.onItemOpen2}
+            />
+            <Tab
+                classList={['Tab-blue', 'full']}
+                tabs={[
+                    {
+                        name   : 'properties',
+                        title  : 'Properties',
+                        content: <PropertyGrid2
                             onCreate={c => ctrl.pg = c}
                             onChange={ctrl.onPropertyGrid2Change}
                         />
-                    </div>
-                }
-            ]}
-        />
+                    }
+                ]}
+            />
+        </div>;
     }
 }
