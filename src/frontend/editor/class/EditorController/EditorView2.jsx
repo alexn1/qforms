519c2c9170f27b7e6e33e3042d49a9553f20243c
Alexander Nesterenko <alexn140@gmail.com>
2021-02-11 01:30:12 +0700

    tab

--- src/frontend/editor/class/EditorController/EditorView2.jsx
+++ src/frontend/editor/class/EditorController/EditorView2.jsx
@@ -1,5 +1,19 @@
 class EditorView2 extends ReactComponent {
     render() {
-        return <div>EditorView2</div>;
+        return <Tab
+            classList={['full']}
+            tabs={[
+                {
+                    name: 'tab1',
+                    title: 'Tab1',
+                    content: <div>tab1 content</div>
+                },
+                {
+                    name: 'tab2',
+                    title: 'Tab2',
+                    content: <div>tab2 content</div>
+                }
+            ]}
+        />;
     }
 }
