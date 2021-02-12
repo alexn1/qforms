18d5c2efcddbd796c54aa414e8024883aaf67954
Alexander Nesterenko <alexn140@gmail.com>
2021-02-12 22:29:25 +0700

    log

--- src/frontend/editor/class/ModelController/DocumentController/DatabaseController/DatabaseView.jsx
+++ src/frontend/editor/class/ModelController/DocumentController/DatabaseController/DatabaseView.jsx
@@ -1,13 +1,16 @@
 class DatabaseView extends ReactComponent {
     renderGrid() {
+        // console.log('DatabaseView.renderGrid');
         const ctrl = this.props.ctrl;
         return <Grid
             columns={Object.keys(ctrl.tableInfo[0]).map(name => ({name: name, title: name, width: 100}))}
             rows={ctrl.tableInfo}
             getRowKey={row => row.name}
+            updated={Date.now()}
         />;
     }
     render() {
+        // console.log('DatabaseView.render');
         const ctrl = this.props.ctrl;
         const document = this.props.document;
         return <div className={'DatabaseView place'}>
