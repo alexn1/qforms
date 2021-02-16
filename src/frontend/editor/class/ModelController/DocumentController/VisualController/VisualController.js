e3b9d1db0b67faac4eb4f4b3fc366710abec5937
Alexander Nesterenko <alexn140@gmail.com>
2021-02-16 15:24:10 +0700

    misc

--- src/frontend/editor/class/ModelController/DocumentController/VisualController/VisualController.js
+++ src/frontend/editor/class/ModelController/DocumentController/VisualController/VisualController.js
@@ -3,17 +3,16 @@ class VisualController extends DocumentController {
     constructor(model, parent) {
         super(model, parent);
         this.data = null;
-        this.$view = null;
-        this.cmTemplateHtml = null;
+        // this.$view = null;
+        /*this.cmTemplateHtml = null;
         this.cmTemplateCss  = null;
-        this.cmTemplateJs   = null;
-        this.save = null;
-        this.tabWidget = null;
-
-        this.cm   = null;
+        this.cmTemplateJs   = null;*/
+        // this.save = null;
+        // this.tabWidget = null;
+        this.cm = null;
     }
 
-    async createTab(docs) {
+    /*async createTab(docs) {
         const name = this.model.getFullName('_');
         const caption = this.model.getFullName('/');
 
@@ -58,9 +57,9 @@ class VisualController extends DocumentController {
             });
             this.$view.find('.btnSaveController').css('display', 'none');
         }
-    }
+    }*/
 
-    tabWidget_TabShow(ea) {
+    /*tabWidget_TabShow(ea) {
         if ($(ea.tab).hasClass('tabController')) {
             if (this.data.js) {
                 if (this.cmTemplateJs === null) {
@@ -68,9 +67,9 @@ class VisualController extends DocumentController {
                 }
             }
         }
-    }
+    }*/
 
-    showCustomController() {
+    /*showCustomController() {
         this.$view.find('.wndJs').css('display', 'block');
         if ($(this.tabWidget.activeTab).hasClass('tabController')) {
             this.initCmTemplateJs();
@@ -78,9 +77,9 @@ class VisualController extends DocumentController {
         this.$view.find('.btnSaveController').click(() => {this.btnSaveController_Click();});
         this.$view.find('.btnCreateController').css('display', 'none');
         this.$view.find('.btnSaveController').css('display', 'inline-block');
-    }
+    }*/
 
-    showCustomView() {
+    /*showCustomView() {
         this.$view.find('.wndHtml').css('display', 'block');
         this.initCmTemplateHtml();
         this.$view.find('.btnSaveView').click(() => {this.btnSaveView_Click();});
@@ -88,27 +87,27 @@ class VisualController extends DocumentController {
         this.$view.find('.btnCss').click(() => {this.btnCss_Click();});
         this.$view.find('.btnCreateView').css('display', 'none');
         this.$view.find('.btnSaveView').css('display', 'inline-block');
-    }
+    }*/
 
-    initCmTemplateHtml() {
+    /*initCmTemplateHtml() {
         this.cmTemplateHtml = CodeMirror.fromTextArea(this.$view.find('.cmHtmlView').get(0), {lineNumbers: true, styleActiveLine: true, matchBrackets: true});
         this.cmTemplateHtml.setOption('theme', 'cobalt');
         this.cmTemplateHtml.setValue(this.data.ejs);
-    }
+    }*/
 
-    initCmTemplateCss() {
+    /*initCmTemplateCss() {
         this.cmTemplateCss = CodeMirror.fromTextArea(this.$view.find('.cmCssView').get(0), {lineNumbers: true, styleActiveLine: true, matchBrackets: true});
         this.cmTemplateCss.setOption('theme', 'cobalt');
         this.cmTemplateCss.setValue(this.data.css);
-    }
+    }*/
 
-    initCmTemplateJs() {
+    /*initCmTemplateJs() {
         this.cmTemplateJs = CodeMirror.fromTextArea(this.$view.find('.cmJsCode').get(0), {lineNumbers: true, styleActiveLine: true, matchBrackets: true});
         this.cmTemplateJs.setOption('theme', 'cobalt');
         this.cmTemplateJs.setValue(this.data.js);
-    }
+    }*/
 
-    btnSaveView_Click() {
+    /*btnSaveView_Click() {
         let text;
         switch (this.save) {
             case 'ejs':
@@ -119,14 +118,14 @@ class VisualController extends DocumentController {
                 break;
         }
         this.model.saveView(text, this.save);
-    }
+    }*/
 
-    btnSaveController_Click() {
+    /*btnSaveController_Click() {
         const text  = this.cmTemplateJs.getValue();
         this.model.saveController(text);
-    }
+    }*/
 
-    btnHtml_Click() {
+    /*btnHtml_Click() {
         this.$view.find('.wndHtml').css('display', 'block');
         this.$view.find('.wndCss').css('display', 'none');
         this.$view.find('.btnHtml').removeClass('btn-default');
@@ -134,9 +133,9 @@ class VisualController extends DocumentController {
         this.$view.find('.btnCss').removeClass('btn-primary');
         this.$view.find('.btnCss').addClass('btn-default');
         this.save = 'ejs';
-    }
+    }*/
 
-    btnCss_Click() {
+    /*btnCss_Click() {
         this.$view.find('.wndCss').css('display', 'block');
         this.$view.find('.wndHtml').css('display', 'none');
         if (this.cmTemplateCss === null) {
@@ -147,20 +146,20 @@ class VisualController extends DocumentController {
         this.$view.find('.btnHtml').removeClass('btn-primary');
         this.$view.find('.btnHtml').addClass('btn-default');
         this.save = 'css';
-    }
+    }*/
 
-    async btnCreateView_Click() {
+    /*async btnCreateView_Click() {
         const data = await this.model.createView();
         this.data.ejs = data.ejs;
         this.data.css = data.css;
         this.showCustomView();
-    }
+    }*/
 
-    async btnCreateController_Click() {
+    /*async btnCreateController_Click() {
         const data = await this.model.createController();
         this.data.js = data.js;
         this.showCustomController();
-    }
+    }*/
 
     async createDocument() {
         console.log('VisualController.createDocument');
