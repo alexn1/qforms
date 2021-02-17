4b04d49772fc4f770a636356cac60673adb127b7
Alexander Nesterenko <alexn140@gmail.com>
2021-02-17 13:40:59 +0700

    old widgets removed

--- src/frontend/editor/class/Widget/Widget.js
+++
@@ -1,8 +0,0 @@
-class Widget extends EventEmitter {
-    constructor(el) {
-        super();
-        if (!el) throw new Error(`${this.constructor.name}: no el`);
-        this.el = el;
-        this.listeners = {};
-    }
-}
