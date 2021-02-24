b83d16450d263c32c310b5d6620f0574544abe5f
Alexander Nesterenko <alexn140@gmail.com>
2021-02-24 13:53:27 +0700

    NewParamView works

--- src/frontend/editor/class/ModalController/NewParamController/NewParamView.jsx
+++ src/frontend/editor/class/ModalController/NewParamController/NewParamView.jsx
@@ -1,5 +1,31 @@
 class NewParamView extends ReactComponent {
+    constructor(props) {
+        super(props);
+        this.name = null;
+    }
+    onCreate = async e => {
+        // console.log('NewParamView.onCreate');
+        await this.props.ctrl.onCreate({
+            name: this.name.getValue()
+        });
+    }
     render() {
-        return <div>abc</div>;
+        const ctrl = this.props.ctrl;
+        return <div className="modal-content" style={{width: 360, margin: 'auto'}}>
+            <div className="modal-header">
+                <button type="button" className="close" onClick={ctrl.onClose}><span>&times;</span></button>
+                <h4 className="modal-title">New Param</h4>
+            </div>
+            <div className="modal-body">
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <TextBox id="name" classList={['form-control']} onCreate={c => this.name = c}/>
+                </div>
+            </div>
+            <div className="modal-footer">
+                <button name="create" type="button" className="btn btn-primary" onClick={this.onCreate}>Create</button>
+                <button type="button" className="btn btn-default" onClick={ctrl.onClose}>Close</button>
+            </div>
+        </div>;
     }
 }
