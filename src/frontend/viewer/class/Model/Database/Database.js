79b8c683a6fa2d8321d3e1cee7108e2a9b5a76f4
Alexander Nesterenko <alexn140@gmail.com>
2021-03-09 14:07:03 +0700

    tables array

--- src/frontend/viewer/class/Model/Database/Database.js
+++ src/frontend/viewer/class/Model/Database/Database.js
@@ -1,19 +1,21 @@
 class Database extends Model {
     constructor(...args) {
         super(...args);
-        this.tables = {};
+        this.tables = [];
     }
 
     init() {
         // console.log('Database.init', this.getName());
         for (const data of this.data.tables) {
-            this.tables[data.name] = new Table(data, this);
-            this.tables[data.name].init();
+            const table = new Table(data, this);
+            table.init();
+            this.tables.push(table);
         }
     }
 
     getTable(name) {
-        if (!this.tables[name]) throw new Error(`no table with name: ${name}`);
-        return this.tables[name];
+        const table = this.tables.find(table => table.getName() === name);
+        if (!table) throw new Error(`no table with name: ${name}`);
+        return table;
     }
 }
