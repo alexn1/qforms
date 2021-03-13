34812a528d84588e40216bc9fd1a0a68b3cd2d0c
Alexander Nesterenko <alexn140@gmail.com>
2021-03-13 12:04:28 +0700

    ref

--- src/backend/editor/EditorController/ActionEditorController/ActionEditorController.js
+++ src/backend/editor/EditorController/ActionEditorController/ActionEditorController.js
@@ -9,15 +9,14 @@ class ActionEditorController extends EditorController {
     async _new(params) {
         console.log('ActionEditorController._new');
         if (!params.pageFileName) throw new Error('no pageFileName');
-        if (!params.form) throw new Error('no form');
-        if (!params.name) throw new Error('no name');
-        // if (!params.caption) throw new Error('no caption');
+        if (!params.form)         throw new Error('no form');
+        if (!params.name)         throw new Error('no name');
         const appEditor = await this.createApplicationEditor();
         const pageEditor = await appEditor.createPageEditor(params.pageFileName);
         const formEditor = pageEditor.createFormEditor(params.form);
-        const actionData = await formEditor.newActionData(params);
-        await formEditor.save();
-        return actionData;
+        const data = await formEditor.newActionData(params);
+        await pageEditor.save();
+        return data;
     }
 
     async save(params) {
@@ -26,6 +25,7 @@ class ActionEditorController extends EditorController {
         const formEditor = pageEditor.createFormEditor(params.form);
         const actionEditor = formEditor.createActionEditor(params.action);
         await actionEditor.setAttr(params.attr, params.value);
+        await pageEditor.save();
         return null;
     }
 
