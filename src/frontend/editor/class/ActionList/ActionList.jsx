18d969b208116887d962a1077afcc8f6d7dfbd07
Alexander Nesterenko <alexn140@gmail.com>
2021-03-31 00:32:14 +0700

    ref

--- src/frontend/editor/class/ActionList/ActionList.jsx
+++ src/frontend/editor/class/ActionList/ActionList.jsx
@@ -5,13 +5,13 @@ class ActionList extends ReactComponent {
             item: null
         };
     }
-    onActionClick = async li => {
-        console.log('ActionList.onActionClick', li);
+    onClick = async li => {
+        console.log('ActionList.onClick', li);
         await this.props.ctrl.onActionClick(li.dataset.action);
     }
     render() {
         // console.log('ActionList.render', this.state.item);
-        return <DropdownButton onClick={this.onActionClick} actions={this.state.item ? this.state.item.getActions().map(action => {
+        return <DropdownButton onClick={this.onClick} actions={this.state.item ? this.state.item.getActions().map(action => {
             return {name: action.action, title: action.caption};
         }) : []}>Actions</DropdownButton>
     }
