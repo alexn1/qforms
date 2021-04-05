332e95655d7d378f2aa469123ff02ab9cb208651
Alexander Nesterenko <alexn140@gmail.com>
2021-04-05 11:28:23 +0700

    ref

--- src/backend/editor/Editor/FieldEditor/FieldEditor.js
+++ src/backend/editor/Editor/FieldEditor/FieldEditor.js
@@ -45,14 +45,14 @@ class FieldEditor extends Editor {
     static createAttributes(params) {
         return {
             name        : params.name,
-            caption     : params.caption      ? params.caption      :    params.name,
-            column      : params.column       ? params.column       :    params.name,
-            value       : params.value        ? params.value        :             '',
-            defaultValue: params.defaultValue ? params.defaultValue :             '',
-            param       : params.param        ? params.param        :        'false',
-            isVisible   : params.isVisible    ? params.isVisible    :         'true',
-            type        : params.type         ? params.type         :             '',
-            width       : params.width        ? params.width        :            '0',
+            caption     : params.caption      !== undefined ? params.caption : params.name,
+            column      : params.column       !== undefined ? params.column       :    params.name,
+            value       : params.value        !== undefined ? params.value        :             '',
+            defaultValue: params.defaultValue !== undefined ? params.defaultValue :             '',
+            param       : params.param        !== undefined ? params.param        :        'false',
+            isVisible   : params.isVisible    !== undefined ? params.isVisible    :         'true',
+            type        : params.type         !== undefined ? params.type         :             '',
+            width       : params.width        !== undefined ? params.width        :            '0',
         }
     }
 
