c103b65021111bbbcfeaed9f018f91c3780638aa
Alexander Nesterenko <alexn140@gmail.com>
2021-05-15 03:22:49 +0700

    slider full screen

--- src/frontend/common/jsx/widget/Slider/Slider.jsx
+++ src/frontend/common/jsx/widget/Slider/Slider.jsx
@@ -3,6 +3,7 @@ class Slider extends ReactComponent {
         super(props);
         if (!this.props.images) throw new Error('Slider: no images');
         this.state = {image: 0};
+        this.el = React.createRef();
     }
     onPrevClick = e => {
         // console.log('Slider.onPrevClick');
@@ -24,13 +25,24 @@ class Slider extends ReactComponent {
             return {image};
         });
     }
+    onImageClick = e => {
+        console.log('Slider.onImageClick');
+        // if (!this.state.full) {
+        //     this.setState({full: true});
+        // }
+        this.el.current.classList.add('Slider_full');
+    }
+    onCloseClick = e => {
+        this.el.current.classList.remove('Slider_full');
+    }
     render() {
         const images = this.props.images || [];
-        return <div className={this.getClassName()} >
-            <img className={'Slider_image'} src={images[this.state.image]}/>
+        return <div className={this.getClassName()} ref={this.el}>
+            <img className={'Slider_image'} src={images[this.state.image]} onClick={this.onImageClick}/>
             <p className={'Slider__label'}>{images.length > 0 ? this.state.image+1 : 0}/{images.length}</p>
             <button className={'Slider__prev'} onClick={this.onPrevClick}>prev</button>
             <button className={'Slider__next'} onClick={this.onNextClick}>next</button>
+            <button className={'Slider__close'} onClick={this.onCloseClick}>close</button>
         </div>;
     }
 }
