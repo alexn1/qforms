263874ce6d6cd1d31efeb5b947969ea55991ba49
Alexander Nesterenko <alexn140@gmail.com>
2021-05-16 01:55:55 +0700

    PageView2

---
+++ src/frontend/viewer/class/Controller/PageController/PageView2.jsx
@@ -0,0 +1,30 @@
+class PageView2 extends PageView {
+    getAllTabs() {
+        const ctrl = this.props.ctrl;
+        return ctrl.forms.map(form => {
+            return {
+                name   : form.model.getName(),
+                title  : form.model.getCaption(),
+                content: PageView.renderForm(form)
+            };
+        });
+    }
+    render() {
+        console.log('PageView2.render', this.props.ctrl.model.getFullName());
+        const ctrl = this.props.ctrl;
+        const model = ctrl.model;
+        return (
+            <div className="PageView full frame">
+                <div className="frame__content flex-rows">
+                    <h3 className="PageView__caption">{this.renderCaption()}</h3>
+                    {(model.hasRowFormWithDefaultDs() || model.hasActions()) && this.renderToolbar()}
+                    <div className="PageView__table-forms flex-max frame">
+                        <div className="frame__content">
+                            <Tab tabs={this.getAllTabs()} classList={['Tab-blue', 'full']}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
