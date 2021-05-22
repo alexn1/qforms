01856442446db0ef3ec4cbd01ad471b0aacec0fb
Alexander Nesterenko <alexn140@gmail.com>
2021-05-22 20:34:41 +0700

    backend/index.js

---
+++ src/backend/index.js
@@ -0,0 +1,4 @@
+module.exports.Helper        = require('./Helper');
+module.exports.Application   = require('./viewer/Model/Application/Application');
+module.exports.Page          = require('./viewer/Model/Page/Page');
+module.exports.SqlDataSource = require('./viewer/Model/DataSource/SqlDataSource/SqlDataSource');
