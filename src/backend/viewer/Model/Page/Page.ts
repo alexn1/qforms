21380902816d3773a2eb4dc052a267fbb26405f5
Alexander Nesterenko <alexn140@gmail.com>
2021-05-24 02:32:52 +0700

    Page.ts ref

--- src/backend/viewer/Model/Page/Page.ts
+++ src/backend/viewer/Model/Page/Page.ts
@@ -3,6 +3,7 @@ const path = require('path');
 import Model from '../Model';
 import DataSource from '../DataSource/DataSource';
 import Action from '../Action/Action';
+import Application from "../Application/Application";
 
 class Page extends Model {
     dataSources: DataSource[];
@@ -16,17 +17,17 @@ class Page extends Model {
         this.forms       = [];
     }
 
-    async init() {
+    async init(): Promise<void> {
         await this.createColItems('dataSources');
         await this.createColItems('actions');
         await this.createColItems('forms');
     }
 
-    getDirPath() {
+    getDirPath(): string {
         return path.join(this.parent.getDirPath(), 'pages', this.getName());
     }
 
-    async fill(context) {
+    async fill(context): Promise<any> {
         // console.log('Page.fill', this.constructor.name, this.getFullName());
         const response = await super.fill(context);
         await this.fillCollection(response, 'dataSources', context);
@@ -40,24 +41,24 @@ class Page extends Model {
         return response;
     }
 
-    async rpc(name, context) {
+    async rpc(name, context): Promise<any> {
         console.log('Page.rpc', name, context.params);
         if (this[name]) return await this[name](context);
         return {errorMessage: `no rpc ${name}`};
     }
 
-    getApp() {
+    getApp(): Application {
         return this.parent;
     }
 
-    getTitle() {
+    getTitle(): string {
         return this.getAttr('caption');
     }
     getForm(name) {
         return this.forms.find(form => form.getName() === name);
     }
 
-    getDataSource(name) {
+    getDataSource(name): DataSource {
         return this.dataSources.find(dataSource => dataSource.getName() === name);
     }
 }
