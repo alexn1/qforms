167ea5fbddf85b55a76161d6de58e12b588f4525
Alexander Nesterenko <alexn140@gmail.com>
2021-05-24 01:26:53 +0700

    Action.ts ref

--- src/backend/viewer/Model/Action/Action.ts
+++ src/backend/viewer/Model/Action/Action.ts
@@ -1,18 +1,9 @@
-// import Application from '../Application/Application';
-const Model = require('../Model');
+import Model from '../Model';
 
 class Action extends Model {
-    constructor(data: any, parent: any) {
-        super(data, parent);
-    }
     static async create(data, parent): Promise<Action> {
         return new Action(data, parent);
     }
-    /*public getApp(): Application {
-        if (this.parent instanceof Application) return this.parent;
-        if (this.parent.parent instanceof Application) return this.parent.parent;
-        if (this.parent.parent.parent instanceof Application) return this.parent.parent.parent;
-    }*/
 }
 
 export = Action;
