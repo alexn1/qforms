842cb00921b5b876e94916ad929d418ae65daa41
Alexander Nesterenko <alexn140@gmail.com>
2021-05-26 22:29:42 +0700

    fix

--- gulp/frontend_editor.js
+++ gulp/frontend_editor.js
@@ -10,8 +10,38 @@ const babel      = require('gulp-babel');
 const BUILD_PATH = './build';
 const SRC_PATH   = "./src";
 
-function frontend_editor_class_css() {
-    return gulp.src(path.join(SRC_PATH, 'frontend/editor/class/**/*.less'))
+
+
+/*function frontend_editor_class_html() {
+    return gulp.src(path.join(SRC_PATH, 'frontend/editor/!**!/!*.html'))
+        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/editor/class')));
+}*/
+
+/*function frontend_editor_class_ejs() {
+    return gulp.src(path.join(SRC_PATH, 'frontend/editor/!**!/!*.ejs'))
+        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/editor/class')));
+}*/
+
+function frontend_editor_js() {
+    return gulp.src(path.join(SRC_PATH, 'frontend/editor/**/*.js'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('editor.js'))
+        //.pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/js')));
+}
+
+function frontend_editor_jsx() {
+    return gulp.src(path.join(SRC_PATH, 'frontend/editor/**/*.jsx'))
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(concat('editor-jsx.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/js')));
+}
+
+function frontend_editor_less() {
+    return gulp.src(path.join(SRC_PATH, 'frontend/editor/**/*.less'))
         .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(concat('editor.css'))
@@ -20,59 +50,10 @@ function frontend_editor_class_css() {
         .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/css')));
 }
 
-function frontend_editor_class_html() {
-    return gulp.src(path.join(SRC_PATH, 'frontend/editor/class/**/*.html'))
-        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/editor/class')));
-}
-
-function frontend_editor_class_ejs() {
-    return gulp.src(path.join(SRC_PATH, 'frontend/editor/class/**/*.ejs'))
-        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/editor/class')));
-}
-
-function frontend_editor_class_js() {
-    return gulp.src(path.join(SRC_PATH, 'frontend/editor/class/**/*.js'))
-        .pipe(sourcemaps.init())
-        .pipe(concat('editor.js'))
-        //.pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/js')));
-}
-
-function frontend_editor_jsx_js() {
-    return gulp.src(path.join(SRC_PATH, 'frontend/editor/class/**/*.jsx'))
-        .pipe(sourcemaps.init())
-        .pipe(babel())
-        .pipe(concat('editor-jsx.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/js')));
-}
-
-function frontend_editor_jsx_less() {
-    return gulp.src(path.join(SRC_PATH, 'frontend/editor/class/**/*.less'))
-        .pipe(sourcemaps.init())
-        .pipe(less())
-        .pipe(concat('editor-jsx.css'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/css')));
-}
-
-
-/*function frontend_editor_lib() {
-    return gulp.src(path.join(SRC_PATH, 'frontend/editor/lib/!**!/!*'))
-        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/frontend/lib')));
-}*/
-
-
 const frontend_editor = gulp.series(
-    frontend_editor_class_js,
-    frontend_editor_class_css,
-    frontend_editor_class_html,
-    frontend_editor_class_ejs,
-    frontend_editor_jsx_js,
-    frontend_editor_jsx_less,
-    /*frontend_editor_lib,*/
-
+    frontend_editor_js,
+    frontend_editor_jsx,
+    frontend_editor_less,
 );
 
 module.exports = frontend_editor;
