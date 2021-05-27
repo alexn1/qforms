1e3251e8f00b8e056ed359858a6ea364acfdfaa2
Alexander Nesterenko <alexn140@gmail.com>
2021-05-27 18:02:13 +0700

    clean

--- gulp/build.js
+++ gulp/build.js
@@ -5,4 +5,9 @@ const root     = require("./root");
 const backend  = require("./backend");
 const frontend = require("./frontend");
 
-module.exports = gulp.series(/*clean, */root, backend, frontend);
+module.exports = gulp.series(
+    clean,
+    root,
+    backend,
+    frontend
+);
