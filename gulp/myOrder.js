6b52ad4cba3e0bd390e80e04d024e3302496b459
Alexander Nesterenko <alexn140@gmail.com>
2021-06-10 01:00:19 +0700

    myOrder

--- gulp/myOrder.js
+++ gulp/myOrder.js
@@ -1,5 +1,42 @@
 const through = require('through');
 
+class Folder {
+    constructor(name) {
+        this.name = name;
+        this.folders = {};
+        this.files   = {};
+    }
+    addFolder(folder) {
+        return this.folders[folder.name] = folder;
+    }
+    addFile(name, file) {
+        this.files[name] = file;
+    }
+}
+
+const rootFolder = new Folder('root');
+
+function placeFile(file) {
+    console.log('placeFile', file.name);
+    const items = file.path.split('/');
+    let folder = rootFolder;
+    for (let i = 1; i <= items.length-2; i++) {
+        const name = items[i];
+        console.log('name:', name);
+
+
+        if (folder.folders[name]) {
+            folder = folder.folders[name];
+        } else {
+            folder = folder.addFolder(new Folder(name));
+        }
+        if (i === items.length-2) {
+            folder.addFile(items[items.length-1], file);
+        }
+    }
+
+}
+
 module.exports = function () {
     const files = [];
     const onFile = function(file) {
@@ -9,6 +46,31 @@ module.exports = function () {
     const onEnd = function() {
         console.log('onEnd');
         // files.reverse();
+
+        // const file = files[0];
+
+        for (const file of files) {
+            // console.log('file.path:', file.path.split('/'));
+            placeFile(file);
+        }
+
+        console.log(
+            rootFolder
+                .folders['home']
+                .folders['user']
+                .folders['projects']
+                .folders['qforms']
+                .folders['src']
+                .folders['frontend']
+                .folders['viewer']
+        );
+
+
+
+
+
+
+
         files.forEach((file) => {
             return this.emit("data", file);
         });
