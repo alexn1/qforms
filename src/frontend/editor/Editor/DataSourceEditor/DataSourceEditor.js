5b3e97134b84fa8c4278744375b06c83b9009d1f
Alexander Nesterenko <alexn140@gmail.com>
2021-06-15 10:56:32 +0700

    fix

--- src/frontend/editor/Editor/DataSourceEditor/DataSourceEditor.js
+++ src/frontend/editor/Editor/DataSourceEditor/DataSourceEditor.js
@@ -86,9 +86,13 @@ class DataSourceEditor extends Editor {
             controller: 'DataSource',
             action    : 'createModelBackJs',
             params    : Helper.encodeObject({
-                ...(this.parent instanceof PageEditor ? {pageFileName: this.parent.pageLink.getFileName()} : {}),
+                ...(this.parent instanceof PageEditor ? {
+                    page        : this.parent.getName(),
+                    pageFileName: this.parent.pageLink.getFileName()
+                } : {}),
                 ...(this.parent instanceof FormEditor ? {
                     form        : this.parent.getName(),
+                    page        : this.parent.page.getName(),
                     pageFileName: this.parent.page.pageLink.getFileName()
                 } : {}),
                 dataSource: this.getName(),
