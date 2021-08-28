e5eea5f1356fe6316a6c5b14480275f754bd9d8b
Alexander Nesterenko <alexn140@gmail.com>
2021-08-28 12:56:39 +0700

    timezone attr

--- src/backend/editor/Editor/FieldEditor/DateTimeFieldEditor/DateTimeFieldEditor.js
+++ src/backend/editor/Editor/FieldEditor/DateTimeFieldEditor/DateTimeFieldEditor.js
@@ -6,9 +6,10 @@ class DateTimeFieldEditor extends FieldEditor {
             '@class'     : 'DateTimeField',
             '@attributes': {
                 ...FieldEditor.createAttributes(params),
-                readOnly        : params.readOnly         ? params.readOnly     :                           'false',
-                notNull         : params.notNull          ? params.notNull      :                           'false',
+                readOnly        : params.readOnly         ? params.readOnly        :                           'false',
+                notNull         : params.notNull          ? params.notNull         :                           'false',
                 format          : params.format           ? params.format          : '{DD}.{MM}.{YYYY} {hh}:{mm}:{ss}',
+                timezone        : params.timezone         ? params.timezone        :                            'true',
                 placeholder     : params.placeholder      ? params.placeholder     :                                '',
                 validateOnChange: params.validateOnChange ? params.validateOnChange:                            'true',
                 validateOnBlur  : params.validateOnBlur   ? params.validateOnBlur  :                           'false',
