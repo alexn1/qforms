cdadb9ba78848d3daeb133b367e8f773b4efdc51
Alexander Nesterenko <alexn140@gmail.com>
2021-08-30 13:43:50 +0700

    date pattern

--- test/test.js
+++ test/test.js
@@ -1,27 +1,20 @@
-const Helper =  require('../dist/lib/backend/Helper');
-
-function templateToJsString(value, params) {
-    return value.replace(/\$\{([\w\.@]+)\}/g, (text, name) => {
-        if (params.hasOwnProperty(name)) {
-            return `Helper.decodeValue('${Helper.encodeValue(params[name])}')`;
-        }
-        return 'undefined';
-    });
+const Helper = require('../dist/lib/backend/Helper');
+function dateTimeReviver(key, value) {
+    if (typeof value === 'string') {
+        // x* опоставляется с предшествующим элементом x ноль или более раз.
+        // x? Сопоставляется с предшествующим элементом x ноль или один раз.
+        const a =
+            /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})(\.\d{3})?(Z|([+\-])(\d{2}):(\d{2}))?$/
+                .exec(value);
+        console.log('a:', a);
+        if (a) return new Date(value);
+    }
+    return value;
 }
 
 main(); async function main() {
-    /*try {
-        const filePath = '/home/user/projects/car/apps/site/pages/Car/forms/Car/Controller.js';
-        await Helper.writeFile2(filePath, 'abc');
-    } catch (err) {
-        console.error(err);
-    }*/
-    const js = templateToJsString('${abc} + ${xyz}', {abc: 5, xyz: 6});
-    console.log('js:', js);
-    console.log('value:', eval(js));
-
-
-
-
-
+    // const json = '"2021-04-08T09:45:48.594Z"';
+    const json = '"2021-08-08T12:00:00"';
+    const value = JSON.parse(json, dateTimeReviver);
+    console.log('value:', value);
 }
