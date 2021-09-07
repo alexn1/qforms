94cfbc2d590b843773f42b3e9a78bf3fb80a7fe4
Alexander Nesterenko <alexn140@gmail.com>
2021-09-07 16:29:37 +0700

    misc

--- src/frontend/viewer/Controller/FormController/TableFormController/TableFormView.jsx
+++ src/frontend/viewer/Controller/FormController/TableFormController/TableFormView.jsx
@@ -99,7 +99,7 @@ class TableFormView extends FormView {
         console.log('TableFormView.render', this.props.ctrl.model.getFullName());
         const ctrl = this.props.ctrl;
         return (
-            <div className="TableFormView full flex-rows">
+            <div className={'TableFormView full flex-rows'}>
                 {this.renderToolbar()}
                 <Grid
                     classList={['flex-max']}
