fde72e118067bccd43d09c45dc766ddffccacbae
Alexander Nesterenko <alexn140@gmail.com>
2021-09-29 16:04:31 +0700

    on server socket close

--- dist/lib/backend/WebSocketServer.d.ts
+++ dist/lib/backend/WebSocketServer.d.ts
@@ -4,5 +4,7 @@ declare class WebSocketServer {
     constructor(options: any);
     onError(err: any): void;
     onConnection(webSocket: any): void;
+    onClose(webSocket: any, code: any, reason: any): void;
+    onMessage(webSocket: any, data: any, flags: any): void;
 }
 export = WebSocketServer;
--- src/backend/WebSocketServer.ts
+++ src/backend/WebSocketServer.ts
@@ -20,16 +20,31 @@ class WebSocketServer {
     onConnection(webSocket) {
         console.log('WebSocketServer.onConnection', webSocket.upgradeReq.url);
         const parts = url.parse(webSocket.upgradeReq.url, true);
-        const route = parts.query.route;
-        if (!route) throw new Error('no route');
-        const uuid = parts.query.uuid;
-        if (!uuid) throw new Error('no uuid');
-        webSocket.uuid = uuid;
-        if (!this.clients[route]) {
-            this.clients[route] = [];
-        }
-        this.clients[route].push(webSocket);
+        if (!parts.query.route) throw new Error('no route');
+        if (!parts.query.uuid) throw new Error('no uuid');
+        webSocket.route = parts.query.route;
+        webSocket.uuid = parts.query.uuid;
+        webSocket.on('close', this.onClose.bind(this, webSocket));
+        webSocket.on('message', this.onMessage.bind(this, webSocket));
+
+        // add to clients
+        if (!this.clients[webSocket.route]) this.clients[webSocket.route] = [];
+        this.clients[webSocket.route].push(webSocket);
+
+        // say hello
         webSocket.send(`hello ${webSocket.uuid}`);
+        // console.log('this.clients', this.clients);
+    }
+    onClose(webSocket, code, reason) {
+        console.log('WebSocketServer.onSocketClose', webSocket.route, webSocket.uuid, code, reason);
+        const i = this.clients[webSocket.route].indexOf(webSocket);
+        if (i === -1) throw new Error(`cannot find socket: ${webSocket.route} webSocket.uuid`);
+        console.log('i:', i);
+        this.clients[webSocket.route].splice(i, 1);
+        console.log('this.clients', this.clients);
+    }
+    onMessage(webSocket, data, flags) {
+        console.log('WebSocketServer.onMessage', webSocket.route, webSocket.uuid, data, flags);
     }
 }
 
