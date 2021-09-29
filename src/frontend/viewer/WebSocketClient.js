094edaa162253711c8116193b26785dc20d87d93
Alexander Nesterenko <alexn140@gmail.com>
2021-09-30 04:08:11 +0700

    wait 10 sec for second reconnect

--- src/frontend/viewer/WebSocketClient.js
+++ src/frontend/viewer/WebSocketClient.js
@@ -11,7 +11,7 @@ class WebSocketClient {
         console.log('WebSocketClient.connect', this.url);
         return new Promise((resolve, reject) => {
             this.webSocket = new WebSocket(this.url);
-            this.webSocket.onclose = e => {
+            this.webSocket.onclose = async e => {
                 this.webSocket = null;
                 reject(new Error(`Connection failed ${e.code}`));
             };
@@ -23,29 +23,30 @@ class WebSocketClient {
             };
         });
     }
-    /*onError(e) {
+    /*async onError(e) {
         console.log('WebSocketClient.onError', e);
     }*/
-    reconnect() {
+    async reconnect() {
         console.log(`WebSocketClient.reconnect, waiting ${this.RECONNECT_TIMEOUT} sec`);
-        setTimeout(async () => {
-            try {
-                await this.connect();
-            } catch (err) {
-                console.error(err);
-                this.reconnect();
-            }
-        }, this.RECONNECT_TIMEOUT * 1000);
+        try {
+            await this.connect();
+        } catch (err) {
+            console.error(err);
+            console.log(`waiting ${this.RECONNECT_TIMEOUT} sec...`);
+            setTimeout(async () => {
+                await this.reconnect();
+            }, this.RECONNECT_TIMEOUT * 1000);
+        }
     }
-    onClose(e) {
+    async onClose(e) {
         console.error('WebSocketClient.onClose', e);
         // this.webSocket.onerror = null;
         this.webSocket.onclose = null;
         this.webSocket.onmessage = null;
         this.webSocket = null;
-        this.reconnect();
+        await this.reconnect();
     }
-    onMessage(e) {
+    async onMessage(e) {
         console.log('WebSocketClient.onMessage', JSON.parse(e.data));
         const packet = JSON.parse(e.data);
         if (packet.type === 'result') {
