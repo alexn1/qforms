e4855a91f849308de7a8dab56f7405b66847fc0d
Alexander Nesterenko <alexn140@gmail.com>
2021-09-30 01:19:24 +0700

    reconnect

--- src/frontend/viewer/WebSocketClient.js
+++ src/frontend/viewer/WebSocketClient.js
@@ -5,6 +5,7 @@ class WebSocketClient {
         if (!options.application) throw new Error('no application');
         this.url = `ws://${window.location.host}/?route=${encodeURIComponent(options.route)}&uuid=${encodeURIComponent(options.uuid)}`;
         this.webSocket = null;
+        this.RECONNECT_TIMEOUT = 10;
     }
     connect() {
         console.log('WebSocketClient.connect', this.url);
@@ -25,12 +26,24 @@ class WebSocketClient {
     /*onError(e) {
         console.log('WebSocketClient.onError', e);
     }*/
+    reconnect() {
+        console.log(`WebSocketClient.reconnect, waiting ${this.RECONNECT_TIMEOUT} sec`);
+        setTimeout(async () => {
+            try {
+                await this.connect();
+            } catch (err) {
+                console.error(err);
+                this.reconnect();
+            }
+        }, this.RECONNECT_TIMEOUT * 1000);
+    }
     onClose(e) {
         console.error('WebSocketClient.onClose', e);
         // this.webSocket.onerror = null;
         this.webSocket.onclose = null;
         this.webSocket.onmessage = null;
         this.webSocket = null;
+        this.reconnect();
     }
     onMessage(e) {
         console.log('WebSocketClient.onMessage', JSON.parse(e.data));
