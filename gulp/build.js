264e6bdc5dd1f8ed862d5a5e98a64e1216c7a35d
Alexander Nesterenko <alexn140@gmail.com>
2021-10-10 02:04:05 +0700

    misc

--- gulp/build.js
+++ gulp/build.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 
-const clean    = require('./clean');
+// const clean    = require('./clean');
 const root     = require("./root");
 const backend  = require("./backend");
 const frontend = require("./frontend");
