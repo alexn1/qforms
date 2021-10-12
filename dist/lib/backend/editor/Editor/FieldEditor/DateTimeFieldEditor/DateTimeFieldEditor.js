71863810890e6adadb036a0ca437f50002b1c0e8
Alexander Nesterenko <alexn140@gmail.com>
2021-10-12 20:02:21 +0700

    release v0.0.100

--- dist/lib/backend/editor/Editor/FieldEditor/DateTimeFieldEditor/DateTimeFieldEditor.js
+++ dist/lib/backend/editor/Editor/FieldEditor/DateTimeFieldEditor/DateTimeFieldEditor.js
@@ -4,7 +4,7 @@ class DateTimeFieldEditor extends FieldEditor {
     static createData(params) {
         return {
             '@class': 'DateTimeField',
-            '@attributes': Object.assign(Object.assign({}, FieldEditor.createAttributes(params)), { readOnly: params.readOnly ? params.readOnly : 'false', notNull: params.notNull ? params.notNull : 'false', format: params.format ? params.format : '{DD}.{MM}.{YYYY} {hh}:{mm}:{ss}', timezone: params.timezone ? params.timezone : 'true', placeholder: params.placeholder ? params.placeholder : '', validateOnChange: params.validateOnChange ? params.validateOnChange : 'true', validateOnBlur: params.validateOnBlur ? params.validateOnBlur : 'false' })
+            '@attributes': Object.assign(Object.assign({}, FieldEditor.createAttributes(params)), { readOnly: params.readOnly ? params.readOnly : 'false', notNull: params.notNull ? params.notNull : 'false', format: params.format ? params.format : '{DD}.{MM}.{YYYY}', timezone: params.timezone ? params.timezone : 'true', placeholder: params.placeholder ? params.placeholder : '', validateOnChange: params.validateOnChange ? params.validateOnChange : 'true', validateOnBlur: params.validateOnBlur ? params.validateOnBlur : 'false' })
         };
     }
 }
