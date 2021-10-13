52175b9a86a2d9695186eb7a546a853821dd0aea
Alexander Nesterenko <alexn140@gmail.com>
2021-10-14 03:53:58 +0700

    fix

--- dist/lib/frontend/viewer/js/viewer-jsx.72eba282ae06067e8fb34e4f33751b76.js
+++ dist/lib/frontend/viewer/js/viewer-jsx.72eba282ae06067e8fb34e4f33751b76.js
@@ -1140,6 +1140,7 @@ class PageView extends ModelView {
     return /*#__PURE__*/React.createElement("div", {
       className: `${this.getCssBlockName()}__header`
     }, this.renderTitle(), model.isModal() && [/*#__PURE__*/React.createElement("svg", {
+      key: 'open',
       className: `${this.getCssBlockName()}__open`,
       xmlns: "http://www.w3.org/2000/svg",
       height: "24px",
@@ -1152,6 +1153,7 @@ class PageView extends ModelView {
     }), /*#__PURE__*/React.createElement("path", {
       d: "M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"
     })), /*#__PURE__*/React.createElement("svg", {
+      key: 'close',
       className: `${this.getCssBlockName()}__close`,
       onClick: ctrl.onClosePageClick,
       xmlns: "http://www.w3.org/2000/svg",
