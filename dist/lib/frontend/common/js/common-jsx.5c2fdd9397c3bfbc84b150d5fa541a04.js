e018941d0fdced0f0987b8458f7342dace5f3d30
Alexander Nesterenko <alexn140@gmail.com>
2021-11-01 16:17:06 +0700

    OpenInNewIcon

--- dist/lib/frontend/common/js/common-jsx.5c2fdd9397c3bfbc84b150d5fa541a04.js
+++ dist/lib/frontend/common/js/common-jsx.5c2fdd9397c3bfbc84b150d5fa541a04.js
@@ -128,6 +128,23 @@ class CloseIcon2 extends ReactComponent {
   }
 
 }
+class OpenInNewIcon extends ReactComponent {
+  render() {
+    return /*#__PURE__*/React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      height: "24px",
+      viewBox: "0 0 24 24",
+      width: "24px",
+      fill: "#000000"
+    }, /*#__PURE__*/React.createElement("path", {
+      d: "M0 0h24v24H0V0z",
+      fill: "none"
+    }), /*#__PURE__*/React.createElement("path", {
+      d: "M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"
+    }));
+  }
+
+}
 class DropDownIcon extends ReactComponent {
   render() {
     return /*#__PURE__*/React.createElement("div", {
