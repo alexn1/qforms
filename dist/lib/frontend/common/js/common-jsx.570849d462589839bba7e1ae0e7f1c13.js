6fbf4141b87c99d2f5ff946862d369715103ac3b
Alexander Nesterenko <alexn140@gmail.com>
2021-11-02 16:28:30 +0700

    build

--- dist/lib/frontend/common/js/common-jsx.570849d462589839bba7e1ae0e7f1c13.js
+++ dist/lib/frontend/common/js/common-jsx.570849d462589839bba7e1ae0e7f1c13.js
@@ -128,6 +128,23 @@ class CloseIcon2 extends ReactComponent {
   }
 
 }
+class DateIcon extends ReactComponent {
+  render() {
+    return /*#__PURE__*/React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      height: "18px",
+      viewBox: "0 0 24 24",
+      width: "18px",
+      fill: "#000000"
+    }, /*#__PURE__*/React.createElement("path", {
+      d: "M0 0h24v24H0V0z",
+      fill: "none"
+    }), /*#__PURE__*/React.createElement("path", {
+      d: "M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V10h16v11zm0-13H4V5h16v3z"
+    }));
+  }
+
+}
 class OpenInNewIcon extends ReactComponent {
   render() {
     return /*#__PURE__*/React.createElement("svg", {
