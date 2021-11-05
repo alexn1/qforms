61144654135699fcb7f215f88ced5740fc200fbe
Alexander Nesterenko <alexn140@gmail.com>
2021-11-05 22:27:20 +0700

    toolbar button justify content

--- dist/lib/frontend/common/js/common-jsx.ab213622ffd1d2da86dac9531c677a5a.js
+++ dist/lib/frontend/common/js/common-jsx.ab213622ffd1d2da86dac9531c677a5a.js
@@ -76,6 +76,23 @@ class ReactComponent extends React.Component {
 }
 
 window.QForms.ReactComponent = ReactComponent;
+class AddIcon extends ReactComponent {
+  render() {
+    return /*#__PURE__*/React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      height: "24px",
+      viewBox: "0 0 24 24",
+      width: "24px",
+      fill: "#000000"
+    }, /*#__PURE__*/React.createElement("path", {
+      d: "M0 0h24v24H0V0z",
+      fill: "none"
+    }), /*#__PURE__*/React.createElement("path", {
+      d: "M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"
+    }));
+  }
+
+}
 class ArrowIcon extends ReactComponent {
   render() {
     return /*#__PURE__*/React.createElement("svg", {
@@ -159,6 +176,23 @@ class DateIcon extends ReactComponent {
       fill: "none"
     }), /*#__PURE__*/React.createElement("path", {
       d: "M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V10h16v11zm0-13H4V5h16v3z"
+    }));
+  }
+
+}
+class DeleteIcon extends ReactComponent {
+  render() {
+    return /*#__PURE__*/React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      height: "24px",
+      viewBox: "0 0 24 24",
+      width: "24px",
+      fill: "#000000"
+    }, /*#__PURE__*/React.createElement("path", {
+      d: "M0 0h24v24H0V0z",
+      fill: "none"
+    }), /*#__PURE__*/React.createElement("path", {
+      d: "M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4h-3.5z"
     }));
   }
 
