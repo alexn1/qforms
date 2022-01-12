822264b6ec4d75e3ba24fefafc1f02d34fabbd92
Alexander Nesterenko <alexn140@gmail.com>
2022-01-12 22:22:02 +0700

    gulp inc

--- gulp/inc.js
+++ gulp/inc.js
@@ -1,7 +1,15 @@
 const Lib = require('./Lib');
 
 module.exports = async () => {
+
+    // package.json
     const packageJson = await Lib.getJsonFileData('package.json');
-    packageJson.version = Lib.incPatch(Lib.versionWithoutDev(packageJson.version));
+    const version = packageJson.version = Lib.incPatch(Lib.versionWithoutDev(packageJson.version));
     await Lib.putJsonFileData('package.json', packageJson);
+
+    // package-lock.json
+    const packageLockJson = await Lib.getJsonFileData('package-lock.json');
+    packageLockJson.version = packageLockJson.packages[''].version = version;
+    await Lib.putJsonFileData('package-lock.json', packageLockJson);
+
 }
