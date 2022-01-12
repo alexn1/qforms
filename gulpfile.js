f527c205849f86c49307de41335cf1a6f13d847b
Alexander Nesterenko <alexn140@gmail.com>
2022-01-12 16:56:17 +0700

    test1

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,76 @@
+const fs = require("fs");
+
+function getJsonFileData(filePath) {
+    return new Promise(function(resolve, reject) {
+        fs.readFile(filePath, 'utf8', function(err, text) {
+            if (err) {
+                reject(err);
+            } else {
+                try {
+                    const data = JSON.parse(text);
+                    resolve(data);
+                } catch(err) {
+                    reject(err);
+                }
+            }
+        });
+    });
+}
+
+function versionWithoutDev(version) {
+    const ver = version.split('.');
+    const major = ver[0];
+    const minor = ver[1];
+    const patch = ver[2].split('-');
+    return [major, minor, patch[0]].join('.');
+}
+
+function incPatch(version) {
+    const ver = version.split('.');
+    const major = ver[0];
+    const minor = ver[1];
+    let patch = ver[2];
+    patch = parseInt(patch) + 1;
+    return [major, minor, patch].join('.');
+}
+
+function putJsonFileData(filePath, data) {
+    console.log('putJsonFileData version:', data.version);
+    return new Promise(function(resolve, reject) {
+        try {
+            const text = JSON.stringify(data, null, 2) + '\n';
+            fs.writeFile(filePath, text, function(err) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve();
+                }
+            });
+        } catch(err) {
+            reject(err);
+        }
+    });
+}
+
 module.exports.build = require("./gulp/build");
 module.exports.clean = require('./gulp/clean');
 module.exports.ts = require('./gulp/backend_ts');
+module.exports.test1 = async function () {
+    const packageJson1 = await getJsonFileData('package.json');
+    const version = versionWithoutDev(packageJson1.version);
+
+    console.log('version:', version);
+    const nextVersion = packageJson1.version = incPatch(version);
+
+    console.log('nextVersion:', nextVersion);
+
+    await putJsonFileData('package.json', packageJson1);
+
+
+
+
+};
+
+
 
 module.exports.frontend_viewer = require('./gulp/frontend_viewer');
