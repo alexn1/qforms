c30dc3fa9f48d7b16e723448e39c0d476865a373
Alexander Nesterenko <alexn140@gmail.com>
2022-01-12 14:40:00 +0700

    RowFormLinkFieldView fix

--- src/frontend/viewer/Controller/ModelController/FieldController/RowFormFieldController/RowFormLinkFieldController/RowFormLinkFieldView.jsx
+++ src/frontend/viewer/Controller/ModelController/FieldController/RowFormFieldController/RowFormLinkFieldController/RowFormLinkFieldView.jsx
@@ -3,7 +3,9 @@ class RowFormLinkFieldView extends RowFormFieldView {
         const ctrl = this.props.ctrl;
         return (
             <div className={this.getCssClassNames()}>
-                <a href="#" onClick={ctrl.onClick}>{ctrl.getValueForWidget()}</a>
+                <a href={ctrl.getValueForWidget()} onClick={ctrl.onClick} target={'_blank'}>
+                    {ctrl.getValueForWidget()}
+                </a>
             </div>
         );
     }
