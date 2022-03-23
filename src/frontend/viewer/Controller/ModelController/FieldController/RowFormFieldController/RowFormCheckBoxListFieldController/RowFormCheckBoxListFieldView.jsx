ff93b799f0e9bdd638d1b7990625b898d607894e
Alexander Nesterenko <alexn140@gmail.com>
2022-03-23 13:02:31 +0700

    renderCheckBoxList

--- src/frontend/viewer/Controller/ModelController/FieldController/RowFormFieldController/RowFormCheckBoxListFieldController/RowFormCheckBoxListFieldView.jsx
+++ src/frontend/viewer/Controller/ModelController/FieldController/RowFormFieldController/RowFormCheckBoxListFieldController/RowFormCheckBoxListFieldView.jsx
@@ -1,7 +1,14 @@
 class RowFormCheckBoxListFieldView extends RowFormFieldView {
+    renderCheckBoxList() {
+        return <CheckBoxList
+            classList={[`${this.getCssBlockName()}__checkboxlist`]}
+            name={this.getCtrl().getModel().getFullName()}
+            items={this.getCtrl().getItems()}
+        />;
+    }
     render() {
         return <div className={this.getCssClassNames()}>
-            RowFormCheckBoxListFieldView
+            {this.renderCheckBoxList()}
         </div>;
     }
 }
