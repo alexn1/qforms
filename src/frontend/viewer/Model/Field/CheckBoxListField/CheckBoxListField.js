42b3a985711ac01271bfd4b52c740e8ffcd3ae30
Alexander Nesterenko <alexn140@gmail.com>
2022-03-23 12:31:41 +0700

    CheckBoxListField

---
+++ src/frontend/viewer/Model/Field/CheckBoxListField/CheckBoxListField.js
@@ -0,0 +1,47 @@
+class CheckBoxListField extends Field {
+
+    getDisplayValue(row) {
+        let value = null;
+        if (row[this.data.displayColumn]) {
+            try {
+                value = Helper.decodeValue(row[this.data.displayColumn]);
+            } catch (err) {
+                console.log('cannot parse:', row[this.data.displayColumn]);
+                throw err;
+            }
+        } else {
+            value = this.data.displayColumn;
+            value = value.replace(/\{([\w\.]+)\}/g, (text, name) => {
+                return row.hasOwnProperty(name) ? (row[name] || '') : text;
+            });
+        }
+        return value;
+    }
+
+    getValueValue(row) {
+        if (!row[this.data.valueColumn]) {
+            throw new Error('no valueColumn in CheckBoxList data source');
+        }
+        return Helper.decodeValue(row[this.data.valueColumn]);
+    }
+
+    getDataSource() {
+        const name = this.data.dataSourceName;
+        if (!name) throw new Error(`${this.getFullName()}: no dataSourceName`);
+        if (this.getForm().getDataSource(name)) {
+            return this.getForm().getDataSource(name);
+        }
+        if (this.getPage().getDataSource(name)) {
+            return this.getPage().getDataSource(name);
+        }
+        if (this.getApp().getDataSource(name)) {
+            return this.getApp().getDataSource(name);
+        }
+        throw new Error(`${this.getFullName()}: no data source: ${name}`);
+    }
+
+    findRowByRawValue(rawValue) {
+        return this.getDataSource().getRows().find(row => row[this.data.valueColumn] === rawValue);
+    }
+}
+window.QForms.CheckBoxListField = CheckBoxListField;
