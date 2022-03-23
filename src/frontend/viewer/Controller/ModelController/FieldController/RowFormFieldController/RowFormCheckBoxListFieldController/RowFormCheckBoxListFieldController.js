499166db64913ef777c37391be01dabddcde5c08
Alexander Nesterenko <alexn140@gmail.com>
2022-03-23 12:45:19 +0700

    RowFormComboBoxFieldController

---
+++ src/frontend/viewer/Controller/ModelController/FieldController/RowFormFieldController/RowFormCheckBoxListFieldController/RowFormCheckBoxListFieldController.js
@@ -0,0 +1,47 @@
+class RowFormCheckBoxListFieldController extends RowFormFieldController {
+    init() {
+        // console.log('RowFormCheckBoxListFieldController.init', this.getModel().getFullName());
+        super.init();
+        const dataSource = this.model.getDataSource();
+        dataSource.on('insert', this.onListInsert);
+        dataSource.on('update', this.onListUpdate);
+        dataSource.on('delete', this.onListDelete);
+    }
+    deinit() {
+        const dataSource = this.model.getDataSource();
+        dataSource.off('insert', this.onListInsert);
+        dataSource.off('update', this.onListUpdate);
+        dataSource.off('delete', this.onListDelete);
+        super.deinit();
+    }
+    getViewClass() {
+        return super.getViewClass() || RowFormCheckBoxListFieldView;
+    }
+    getItems() {
+        try {
+            return this.getRows().map(row => ({
+                value: this.model.getValueValue(row).toString(),
+                title: this.model.getDisplayValue(row).toString()
+            }));
+        } catch (err) {
+            err.message = `${this.getModel().getFullName()}: ${err.message}`;
+            throw err;
+        }
+    }
+    getRows() {
+        return this.model.getDataSource().getRows();
+    }
+    onListInsert = async e => {
+        console.log('RowFormCheckBoxListFieldController.onListInsert');
+        await this.rerender();
+    }
+    onListUpdate = async e => {
+        // console.log('RowFormCheckBoxListFieldController.onListUpdate');
+        await this.rerender();
+    }
+    onListDelete = async e => {
+        await this.rerender();
+    }
+}
+
+window.QForms.RowFormCheckBoxListFieldController = RowFormCheckBoxListFieldController;
---
+++ src/frontend/viewer/Controller/ModelController/FieldController/RowFormFieldController/RowFormCheckBoxListFieldController/RowFormCheckBoxListFieldView.jsx
@@ -0,0 +1,7 @@
+class RowFormCheckBoxListFieldView extends RowFormFieldView {
+    render() {
+        return <div className={this.getCssClassNames()}>
+            RowFormCheckBoxListFieldView
+        </div>;
+    }
+}
