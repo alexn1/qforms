7a84c55aa0fdef5a0068fcadec15782d1b7b03f4
Alexander Nesterenko <alexn140@gmail.com>
2022-04-07 14:50:57 +0700

    misc

--- dist/lib/backend/viewer/Model/Field/DateTimeField/DateTimeField.js
+++ dist/lib/backend/viewer/Model/Field/DateTimeField/DateTimeField.js
@@ -10,9 +10,7 @@ class DateTimeField extends Field_1.default {
         response.readOnly = this.getAttr('readOnly');
         response.notNull = this.getAttr('notNull');
         response.format = this.getAttr('format');
-        // if (this.isAttr('timezone')) {
         response.timezone = this.getAttr('timezone');
-        // }
         response.placeholder = this.getAttr('placeholder');
         response.validateOnChange = this.getAttr('validateOnChange');
         response.validateOnBlur = this.getAttr('validateOnBlur');
