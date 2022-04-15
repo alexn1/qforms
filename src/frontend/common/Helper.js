4ecfde63fd7f0b31c5320e6e104964adea2b968b
Alexander Nesterenko <alexn140@gmail.com>
2022-04-15 15:05:50 +0700

    cookie functions

--- src/frontend/common/Helper.js
+++ src/frontend/common/Helper.js
@@ -248,6 +248,30 @@ class Helper {
             return true;
         }
     }
+
+    static setCookie(name, value, time) {
+        var expires = "";
+        if (time) {
+            var date = new Date(time);
+            // date.setTime(date.getTime() + (days*24*60*60*1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (encodeURIComponent(value) || "")  + expires + "; path=/";
+    }
+    static getCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0;i < ca.length;i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1,c.length);
+            if (c.indexOf(nameEQ) == 0) return decodeURIComponent(c.substring(nameEQ.length,c.length));
+        }
+        return undefined;
+    }
+    static eraseCookie(name) {
+        document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
+
 }
 
 window.QForms.Helper = Helper;
