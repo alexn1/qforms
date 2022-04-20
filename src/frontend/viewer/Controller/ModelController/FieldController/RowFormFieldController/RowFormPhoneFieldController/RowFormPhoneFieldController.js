56f02a52dfb8f75fb2a8f93ce665c898b6884f1a
Alexander Nesterenko <alexn140@gmail.com>
2022-04-20 14:42:06 +0700

    phoneNumberFormatError

--- src/frontend/viewer/Controller/ModelController/FieldController/RowFormFieldController/RowFormPhoneFieldController/RowFormPhoneFieldController.js
+++ src/frontend/viewer/Controller/ModelController/FieldController/RowFormFieldController/RowFormPhoneFieldController/RowFormPhoneFieldController.js
@@ -2,5 +2,20 @@ class RowFormPhoneFieldController extends RowFormFieldController {
     getViewClass() {
         return super.getViewClass() || RowFormPhoneFieldView;
     }
+    getPhoneFormatErrorText() {
+        return this.getModel().getApp().getText().form.phoneNumberFormatError;
+    }
+
+    getError() {
+        super.getError();
+
+        // russian phone format validator
+        const value = this.getValue();
+        if (value && value.substr(0,2) === '+7' && value.length < 12) {
+            return this.getPhoneFormatErrorText();
+        }
+
+        return null;
+    }
 }
 window.QForms.RowFormPhoneFieldController = RowFormPhoneFieldController;
