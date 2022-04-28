8615d505fc817925805f4adc7456f12ffa92c626
Alexander Nesterenko <alexn140@gmail.com>
2022-04-28 15:25:48 +0700

    formatTime fixed

--- src/frontend/common/Helper.js
+++ src/frontend/common/Helper.js
@@ -181,10 +181,30 @@ class Helper {
         if (h < 10) h = '0' + h;
         if (m < 10) m = '0' + m;
         if (s < 10) s = '0' + s;
-        if (h === 0) {
+        if (Math.floor(sec / 3600) === 0) {
             return `${sign}${m}:${s}`;
         } else {
             return `${sign}${h}:${m}:${s}`;
+        }
+    }
+    static formatTime2(_sec) {
+        // console.log('Helper.formatTime', sec);
+        let sec = _sec;
+        let sign = '';
+        if (_sec < 0) {
+            sec = -sec;
+            sign = '-';
+        }
+        let h = Math.floor(sec / 3600);
+        let m = Math.floor((sec - h * 3600) / 60);
+        let s = Math.floor(sec - h * 3600 - m * 60);
+        if (h < 10) h = '0' + h;
+        if (m < 10) m = '0' + m;
+        if (s < 10) s = '0' + s;
+        if (Math.floor(sec / 3600) === 0) {
+            return `${sign}${m}m:${s}s`;
+        } else {
+            return `${sign}${h}h:${m}m:${s}s`;
         }
     }
     static SECOND() {
