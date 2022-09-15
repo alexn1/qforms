1491e2fc6fa0238ab1597891459bdec205aa400e
Alexander Nesterenko <alexn140@gmail.com>
2022-09-15 12:14:33 +0700

    webpack/typescript

---
+++ dist2/bundle.js
@@ -0,0 +1,20 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!******************************!*\
+  !*** ./src/backend/start.ts ***!
+  \******************************/
+// import { BackHostApp, Helper } from './index';
+main();
+async function main() {
+    console.log('main');
+    /*const backHostApp = new BackHostApp(Helper.getCommandLineParams());
+    try {
+        await backHostApp.run();
+    } catch (err) {
+        await backHostApp.logError(err);
+    }*/
+}
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUEsWUFBWSxzQkFBc0I7QUFDbEM7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EsTUFBTTtBQUNOO0FBQ0EsS0FBSztBQUNMIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcWZvcm1zLy4vc3JjL2JhY2tlbmQvc3RhcnQudHMiXSwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0IHsgQmFja0hvc3RBcHAsIEhlbHBlciB9IGZyb20gJy4vaW5kZXgnO1xubWFpbigpO1xuYXN5bmMgZnVuY3Rpb24gbWFpbigpIHtcbiAgICBjb25zb2xlLmxvZygnbWFpbicpO1xuICAgIC8qY29uc3QgYmFja0hvc3RBcHAgPSBuZXcgQmFja0hvc3RBcHAoSGVscGVyLmdldENvbW1hbmRMaW5lUGFyYW1zKCkpO1xuICAgIHRyeSB7XG4gICAgICAgIGF3YWl0IGJhY2tIb3N0QXBwLnJ1bigpO1xuICAgIH0gY2F0Y2ggKGVycikge1xuICAgICAgICBhd2FpdCBiYWNrSG9zdEFwcC5sb2dFcnJvcihlcnIpO1xuICAgIH0qL1xufVxuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
