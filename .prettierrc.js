785de9b05b14819bc2f36e9a3fa0c4e0a1a8e231
Alexander Nesterenko <alexn140@gmail.com>
2022-09-15 11:40:01 +0700

    old files removed

--- .prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    tabWidth: 4,
-    singleQuote: true,
-    // trailingComma: "none",
-    // arrowParens: 'always',
-    printWidth: 120,
-    bracketSpacing: false,
-    jsxBracketSameLine: true
-};
