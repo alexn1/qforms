9a39e15b5b8c888f2ed3df80f21ac222d4b67b1f
Alexander Nesterenko <alexn140@gmail.com>
2022-11-30 16:52:35 +0700

    misc

--- src/backend/index/Links.tsx
+++ src/backend/index/Links.tsx
@@ -6,9 +6,9 @@ export const Links = ({links}: LinksProps) => {
     return <>
         {links.map((link, i) => {
             if (typeof link === 'string') {
-                return <link key={i} rel={'stylesheet'} href={link} />
+                return <link key={i} rel={'stylesheet'} href={link} />;
             } else if (typeof link === 'object') {
-                return <link key={i} rel={link.rel} href={link.href} crossOrigin={link.crossorigin ? 'anonymous' : undefined} />
+                return <link key={i} rel={link.rel} href={link.href} crossOrigin={link.crossorigin ? 'anonymous' : undefined} />;
             }
         })}
     </>
