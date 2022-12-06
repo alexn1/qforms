23697569953355951f5ecf762694345c849ab058
Alexander Nesterenko <alexn140@gmail.com>
2022-12-07 01:20:39 +0700

    format

--- src/backend/WebSocketServer.ts
+++ src/backend/WebSocketServer.ts
@@ -1,5 +1,5 @@
-import {BackHostApp} from "./BackHostApp";
-import {Context} from "./Context";
+import { BackHostApp } from './BackHostApp';
+import { Context } from './Context';
 
 const ws = require('ws');
 const url = require('url');
@@ -7,14 +7,15 @@ const url = require('url');
 export class WebSocketServer {
     options: any;
     server: any;
+
     constructor(options) {
         this.options = options;
         // console.log('WebSocketServer.constructor');
         this.server = new ws.Server({
             server: options.httpServer,
-            path: '/'
+            path: '/',
         });
-        this.server.on('error'     , this.onError.bind(this));
+        this.server.on('error', this.onError.bind(this));
         this.server.on('connection', this.onConnection.bind(this));
     }
     async onError(err) {
@@ -26,31 +27,57 @@ export class WebSocketServer {
         // console.log('parts.query:', parts.query);
         if (!parts.query.route) throw new Error('no route');
         if (!parts.query.uuid) throw new Error('no uuid');
-        webSocket.route  = parts.query.route;
-        webSocket.uuid   = parts.query.uuid;
+        webSocket.route = parts.query.route;
+        webSocket.uuid = parts.query.uuid;
         webSocket.userId = parts.query.userId;
         webSocket.customFields = {
-            version: parts.query.version
+            version: parts.query.version,
         };
         webSocket.on('close', this.onClose.bind(this, webSocket));
         webSocket.on('message', this.onMessage.bind(this, webSocket));
 
-        const [appDirName, appFileName, env, domain] = parts.query.route.split('/');
-        const context = new Context({module: 'viewer', appDirName, appFileName, env, domain});
-        const application = await this.getHostApp().createApplicationIfNotExists(context);
+        const [appDirName, appFileName, env, domain] = parts.query.route.split(
+            '/',
+        );
+        const context = new Context({
+            module: 'viewer',
+            appDirName,
+            appFileName,
+            env,
+            domain,
+        });
+        const application = await this.getHostApp().createApplicationIfNotExists(
+            context,
+        );
         application.addClient(webSocket);
 
         // say hello
-        webSocket.send(JSON.stringify({type: 'info', data: {hello: webSocket.uuid}}));
+        webSocket.send(
+            JSON.stringify({ type: 'info', data: { hello: webSocket.uuid } }),
+        );
         // console.log('this.clients', this.clients);
         context.destroy();
     }
     async onClose(webSocket, code, reason) {
-        console.log('WebSocketServer.onSocketClose', webSocket.route, webSocket.uuid, code, reason);
-        this.getHostApp().getApplicationByRoute(webSocket.route).removeClient(webSocket);
+        console.log(
+            'WebSocketServer.onSocketClose',
+            webSocket.route,
+            webSocket.uuid,
+            code,
+            reason,
+        );
+        this.getHostApp()
+            .getApplicationByRoute(webSocket.route)
+            .removeClient(webSocket);
     }
     async onMessage(webSocket, data, flags) {
-        console.log('WebSocketServer.onMessage', webSocket.route, webSocket.uuid, data, flags);
+        console.log(
+            'WebSocketServer.onMessage',
+            webSocket.route,
+            webSocket.uuid,
+            data,
+            flags,
+        );
     }
     getHostApp(): BackHostApp {
         return this.options.hostApp;
