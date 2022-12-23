1eb002465b04729b9cd2bd4bd17429d4e3e27488
Alexander Nesterenko <alexn140@gmail.com>
2022-12-23 17:02:48 +0700

    misc

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
-module.exports.build           = require('./gulp/build');
-module.exports.clean           = require('./gulp/clean');
-module.exports.backend         = require('./gulp/backend');
-module.exports.backend_ts      = require('./gulp/backend_ts');
+module.exports.build = require('./gulp/build');
+module.exports.clean = require('./gulp/clean');
+module.exports.backend = require('./gulp/backend');
+module.exports.backend_ts = require('./gulp/backend_ts');
 module.exports.frontend_viewer = require('./gulp/frontend_viewer');
-module.exports.inc             = require('./gulp/inc');
-module.exports.release         = require('./gulp/release');
-module.exports.publish         = require('./gulp/publish');
+module.exports.inc = require('./gulp/inc');
+module.exports.release = require('./gulp/release');
+module.exports.publish = require('./gulp/publish');
 module.exports.release_publish = require('./gulp/release-publish');
