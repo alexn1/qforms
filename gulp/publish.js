16e95e9411e3491e33d0da67a7b57d47b591ab35
Alexander Nesterenko <alexn140@gmail.com>
2022-12-27 16:30:28 +0700

    publish task

--- gulp/publish.js
+++ gulp/publish.js
@@ -1,9 +1,9 @@
 const Lib = require('./Lib');
 
 async function publish() {
-    await Lib.exec('git checkout release');
+    // await Lib.exec('git checkout release');
     await Lib.exec('cd dist; npm publish');
-    await Lib.exec('git checkout master');
+    // await Lib.exec('git checkout master');
 }
 
 module.exports = publish;
