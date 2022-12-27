8f97fb2b2af6c996475feb0a4376681e01a5a6d7
Alexander Nesterenko <alexn140@gmail.com>
2022-12-27 16:09:31 +0700

    old gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
-module.exports.build = require('./gulp/build');
-module.exports.clean = require('./gulp/clean');
-module.exports.backend = require('./gulp/backend');
-module.exports.backend_ts = require('./gulp/backend_ts');
-module.exports.frontend_viewer = require('./gulp/frontend_viewer');
+// module.exports.build = require('./gulp/build');
+// module.exports.clean = require('./gulp/clean');
+// module.exports.backend = require('./gulp/backend');
+// module.exports.backend_ts = require('./gulp/backend_ts');
+// module.exports.frontend_viewer = require('./gulp/frontend_viewer');
 module.exports.inc = require('./gulp/inc');
 module.exports.release = require('./gulp/release');
 module.exports.publish = require('./gulp/publish');
