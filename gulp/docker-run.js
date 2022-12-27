f37a3526d04ad3fb0fff988f288f68f643ac163f
Alexander Nesterenko <alexn140@gmail.com>
2022-12-27 13:09:10 +0700

    fix

--- gulp/docker-run.js
+++ gulp/docker-run.js
@@ -3,6 +3,6 @@ const Lib = require('./Lib');
 module.exports = async () => {
     const packageJson = await Lib.getJsonFileData('package.json');
     await Lib.exec(
-        `docker run -p 3000:3000 -v qforms-runtime:/app/runtime --rm qforms ${packageJson.version}`,
+        `docker run -p 3000:3000 -v qforms-runtime:/app/runtime --rm qforms:${packageJson.version}`,
     );
 };
