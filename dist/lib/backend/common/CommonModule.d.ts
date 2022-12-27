c11a4ba8553feb11352a2e10dd267234901b9a08
Alexander Nesterenko <alexn140@gmail.com>
2022-12-27 16:44:07 +0700

    old files

--- dist/lib/backend/common/CommonModule.d.ts
+++
@@ -1,8 +0,0 @@
-import { BackHostApp } from '../BackHostApp';
-export declare class CommonModule {
-    hostApp: BackHostApp;
-    css: string[];
-    js: string[];
-    constructor(hostApp: BackHostApp);
-    init(): Promise<void>;
-}
