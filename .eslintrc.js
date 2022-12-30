af61bb85f86fc10af3b5b9d0b7205abf02221935
Alexander Nesterenko <alexn140@gmail.com>
2022-12-30 22:38:07 +0700

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
-    extends: [
-        'eslint:recommended',
-        'airbnb',
-        'airbnb-typescript',
-    ],
+    extends: ['eslint:recommended', 'airbnb', 'airbnb-typescript'],
     parserOptions: {
         project: './tsconfig.json',
     },
+    ignorePatterns: ['.eslintrc.js'],
     rules: {
         'import/prefer-default-export': 'off',
-        '@typescript-eslint/indent': "off",
+        '@typescript-eslint/indent': 'off',
         'no-console': 'off',
         '@typescript-eslint/lines-between-class-members': 'off',
         'arrow-parens': ['warn', 'as-needed'],
-        "react/react-in-jsx-scope": "off",
-        "react/jsx-indent": "off",
-        "react/jsx-indent-props": "off",
-        "spaced-comment": "off",
-        "max-len": "off",
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-indent': 'off',
+        'react/jsx-indent-props': 'off',
+        'spaced-comment': 'off',
+        'max-len': 'off',
     },
 };
