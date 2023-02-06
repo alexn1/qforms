3296f09f52b581c15e089c7c9a78c6063e0b1b44
Alexander Nesterenko <alexn140@gmail.com>
2023-02-06 21:24:45 +0700

    ts

--- src/frontend/viewer/Controller/ModelController/ModelController.ts
+++ src/frontend/viewer/Controller/ModelController/ModelController.ts
@@ -2,30 +2,32 @@ import { Controller } from '../Controller';
 import { ModelView } from './ModelView';
 
 export abstract class ModelController extends Controller {
-    model: any;
-    parent: any;
-    deinited: boolean;
-    constructor(model, parent) {
+    deinited: boolean = false;
+
+    constructor(public model, public parent) {
         super();
-        this.model = model;
-        this.parent = parent;
-        this.deinited = false;
     }
+
     init() {}
+
     deinit() {
         if (this.deinited)
             throw new Error(`${this.model.getFullName()}: controller already deinited`);
         this.deinited = true;
     }
+
     getModel() {
         return this.model;
     }
+
     getParent() {
         return this.parent;
     }
+
     getTitle() {
         return this.getModel().getCaption();
     }
+
     getViewClass(): any {
         // console.log(`${this.constructor.name}.getViewClass`, this.getModel().getAttr('viewClass'));
         const model = this.getModel();
