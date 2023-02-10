428b79a220dc81dde3ae9fa9ad47c8135e89e522
Alexander Nesterenko <alexn140@gmail.com>
2023-02-10 15:20:57 +0700

    docker-run

--- gulp/docker-run.js
+++ gulp/docker-run.js
@@ -4,7 +4,6 @@ module.exports = async () => {
     const packageJson = await Lib.getJsonFileData('package.json');
     // to access host from image network on windows use host.docker.internal
     await Lib.exec(
-        //`docker run -e LISTEN_HOST=0.0.0.0 -p 3000:3000 -v qforms-apps:/app/apps -v qforms-runtime:/app/runtime --rm qforms:${packageJson.version}`,
         `docker run -e LISTEN_HOST=0.0.0.0 -e DB_HOST=host.docker.internal -e APPS_DIR_PATH=/apps -p 3000:3000 -v qforms-apps:/app/apps -v ~/projects/qforms-apps:/apps -v qforms-runtime:/app/runtime --rm qforms:${packageJson.version}`,
     );
 };
