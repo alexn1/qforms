2c8553d75cc6b8bd0e0ad83b7353cc95745697b3
Alexander Nesterenko <alexn140@gmail.com>
2023-02-24 01:04:57 +0700

    def

--- dist/lib/frontend/viewer/Controller/ModelController/PageController/PageView.d.ts
+++ dist/lib/frontend/viewer/Controller/ModelController/PageController/PageView.d.ts
@@ -1,6 +1,7 @@
 import { ModelView } from '../ModelView';
-import {ReactNode} from 'react';
-export declare class PageView extends ModelView {
+import { ReactNode } from 'react';
+import { PageController } from './PageController';
+export declare class PageView extends ModelView<PageController> {
     constructor(props: any);
     onActionsClick: (li: any) => Promise<void>;
     renderHeader(): any;
