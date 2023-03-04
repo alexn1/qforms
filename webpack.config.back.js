d15e1f76556445fa5713ec77e57535a533f74bdd
Alexander Nesterenko <alexn140@gmail.com>
2023-03-04 21:43:34 +0700

    bundle to backend

--- webpack.config.back.js
+++ webpack.config.back.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         // clean: true,
         path: path.resolve(__dirname, './dist/lib'),
-        filename: 'start.js',
+        filename: 'backend/bundle.js',
     },
     mode: process.env.NODE_ENV || 'production',
     devtool: false,
