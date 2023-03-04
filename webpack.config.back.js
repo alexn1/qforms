6a284f9afe3acd3e2e6ab0e7ee91b41ba64c0251
Alexander Nesterenko <alexn140@gmail.com>
2023-03-04 21:32:10 +0700

    start.js

--- webpack.config.back.js
+++ webpack.config.back.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
         // clean: true,
         path: path.resolve(__dirname, './dist/lib'),
-        filename: 'bundle.js',
+        filename: 'start.js',
     },
     mode: process.env.NODE_ENV || 'production',
     devtool: false,
