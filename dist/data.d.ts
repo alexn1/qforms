7a9c86702fa943dabed96aec26575936fef0466c
Alexander Nesterenko <alexn140@gmail.com>
2023-06-03 12:22:02 +0700

    build

---
+++ dist/data.d.ts
@@ -0,0 +1,25 @@
+export interface ApplicationData {
+    class: string;
+    name: string;
+    caption: string;
+    lang: string;
+    theme: string;
+    cssBlock: string;
+    viewClass: string;
+    ctrlClass: string;
+    route: string;
+    domain: string;
+    virtualPath: string;
+    logErrorUrl: string;
+    versions: {
+        platform: string;
+        app: string;
+    };
+    nodeEnv: string;
+    time: number;
+    uuid: string;
+    user: {
+        id: number;
+        login: string;
+    };
+}
