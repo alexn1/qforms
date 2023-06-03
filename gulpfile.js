63b990bce9f7a6f189ab5be29646e84829cb186b
Alexander Nesterenko <alexn140@gmail.com>
2023-06-03 12:02:38 +0700

    old code

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,3 @@
-// module.exports.build = require('./gulp/build');
-// module.exports.clean = require('./gulp/clean');
-// module.exports.backend = require('./gulp/backend');
-// module.exports.backend_ts = require('./gulp/backend_ts');
-// module.exports.frontend_viewer = require('./gulp/frontend_viewer');
-// module.exports.inc = require('./gulp/inc');
-// module.exports.release = require('./gulp/release');
-// module.exports.publish = require('./gulp/publish');
-// module.exports.release_publish = require('./gulp/release-publish');
-// module.exports['backend-ejs'] = require('./gulp/backend-ejs');
-
 module.exports['build-prod'] = require('./gulp/build-prod');
 module.exports['docker-build'] = require('./gulp/docker-build');
 module.exports['docker-run'] = require('./gulp/docker-run');
