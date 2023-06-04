31ee3db12d56a2faa93aea36df4f90876c716a61
Alexander Nesterenko <alexn140@gmail.com>
2023-06-05 01:18:41 +0700

    set release version

--- gulp/release.js
+++ gulp/release.js
@@ -31,6 +31,7 @@ async function release() {
     await Lib.exec('git pull -q origin master');
     await Lib.exec('git push -q origin master');
     const releaseVersion = Lib.versionWithoutDev(await getVersion());
+    await setVersion(releaseVersion);
     // await Lib.exec('npx gulp build-dev');
     if (await isDiff()) {
         await Lib.exec(`git commit -q -am "release v${releaseVersion}"`);
