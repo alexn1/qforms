d6c9243bad5993d4bbc30e44fc9192fa8673a4dd
Alexander Nesterenko <alexn140@gmail.com>
2023-06-10 01:42:58 +0700

    todo deploy script

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 // release
 module.exports['release'] = require('./gulp/release');
 
+// deploy
+// TODO: deploy script
+
 // build
 module.exports['build-dev'] = require('./gulp/build-dev');
 module.exports['build-prod'] = require('./gulp/build-prod');
