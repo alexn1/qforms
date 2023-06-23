88a85a5abe8169f701f66be35ecb61830e3b9079
Alexander Nesterenko <alexn140@gmail.com>
2023-06-23 23:59:43 +0700

    backend ejs gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ module.exports['release'] = require('./gulp/release');
 // publish-npm-package
 module.exports['publish-npm-package'] = require('./gulp/publish-npm-package');
 
+// backend-ejs
+module.exports['backend-ejs'] = require('./gulp/backend-ejs');
+
 // build
 module.exports['build-dev'] = require('./gulp/build-dev');
 module.exports['build-prod'] = require('./gulp/build-prod');
