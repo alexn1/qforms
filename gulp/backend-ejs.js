8f4ad7a59399599ccbf34154e48d8dbfcd8d3491
Alexander Nesterenko <alexn140@gmail.com>
2023-06-24 00:02:25 +0700

    ejs path fixed

--- gulp/backend-ejs.js
+++ gulp/backend-ejs.js
@@ -7,5 +7,5 @@ const BUILD_PATH = './dist';
 module.exports = () => {
     return gulp
         .src(path.join(SRC_PATH, 'backend/**/*.ejs'))
-        .pipe(gulp.dest(path.join(BUILD_PATH, 'lib/backend')));
+        .pipe(gulp.dest(path.join(BUILD_PATH, 'backend')));
 };
