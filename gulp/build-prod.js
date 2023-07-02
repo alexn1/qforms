42e1a7875f05403195431e592b16689b25e235b3
Alexander Nesterenko <alexn140@gmail.com>
2023-07-02 17:28:22 +0700

    copy-lib gulp task

--- gulp/build-prod.js
+++ gulp/build-prod.js
@@ -1,7 +1,8 @@
 const Lib = require('./Lib');
 
 module.exports = async () => {
-    await Lib.exec('npx gulp ejs');
+    await Lib.exec('npx gulp copy-ejs');
+    await Lib.exec('npx gulp copy-lib');
     await Lib.exec('tsc --build tsconfig.back.json');
     await Lib.exec('webpack --config webpack.config.index.js');
     await Lib.exec('webpack --config webpack.config.monitor.js');
--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@ module.exports['release'] = require('./gulp/release');
 // deploy
 module.exports['deploy'] = require('./gulp/deploy');
 
-// ejs
-module.exports['ejs'] = require('./gulp/ejs');
+// copy
+module.exports['copy-ejs'] = require('./gulp/copy-ejs');
+module.exports['copy-lib'] = require('./gulp/copy-lib');
 
 // build
 module.exports['build-dev'] = require('./gulp/build-dev');
