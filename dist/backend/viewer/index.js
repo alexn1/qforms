399e5dba234aae49322e9a71892b79b54068e22d
Alexander Nesterenko <alexn140@gmail.com>
2023-07-08 12:33:49 +0700

    index ref

--- dist/backend/viewer/index.js
+++ dist/backend/viewer/index.js
@@ -1,34 +1,50 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.home = void 0;
-const home = (application, context, applicationController, qformsVersion, links, scripts, data, appViewHtml) => {
-    return `<!DOCTYPE html>
-<html class="${application.getViewClassName()} ${application.getAttr('theme')} ${context.query.debug === '1' ? 'debug' : ''} ${context.query.frame === '1' ? 'iframe' : 'not-iframe'}" lang="${application.getAttr('lang')}">
-<head>
-    <!-- qforms v${qformsVersion} -->
-    <!-- app v${application.getVersion()}  -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>${applicationController.getTitle()}</title>
-    <!-- links -->
-    ${links}
-    <!-- scripts -->
-    ${scripts}
-    <script>
-        document.addEventListener('DOMContentLoaded', async () => {
-            const dataScriptElement = document.querySelector('script[type="application/json"]');
-            const data = JSON.parse(dataScriptElement.textContent);
-            const frontHostApp = new ViewerFrontHostApp({data});
-            frontHostApp.init();
-            await frontHostApp.run();
-        });
-    </script>
-    <script type="application/json">${JSON.stringify(data /*, null, 4*/)}</script>
-</head>
-<body class="${application.getViewClassName()}__body">
-    <div class="${application.getViewClassName()}__root">${appViewHtml}</div>
-    <div class="alert-root"></div>
-</body>
-</html>`;
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
-exports.home = home;
+Object.defineProperty(exports, "__esModule", { value: true });
+__exportStar(require("./BkModel/BkModel"), exports);
+__exportStar(require("./BkModel/BkApplication/BkApplication"), exports);
+__exportStar(require("./BkModel/BkDatabase/BkDatabase"), exports);
+__exportStar(require("./BkModel/BkDatabase/BkSqlDatabase/BkMySqlDatabase/BkMySqlDatabase"), exports);
+__exportStar(require("./BkModel/BkDatabase/BkSqlDatabase/BkPostgreSqlDatabase/BkPostgreSqlDatabase"), exports);
+__exportStar(require("./BkModel/BkDatabase/BkNoSqlDatabase/BkMongoDbDatabase/BkMongoDbDatabase"), exports);
+__exportStar(require("./BkModel/BkDataSource/BkDataSource"), exports);
+__exportStar(require("./BkModel/BkDataSource/BkPersistentDataSource/BkSqlDataSource/BkSqlDataSource"), exports);
+__exportStar(require("./BkModel/BkDataSource/BkPersistentDataSource/BkNoSqlDataSource/BkNoSqlDataSource"), exports);
+__exportStar(require("./BkModel/BkField/BkField"), exports);
+__exportStar(require("./BkModel/BkField/BkCheckBoxField/BkCheckBoxField"), exports);
+__exportStar(require("./BkModel/BkField/BkCheckBoxListField/BkCheckBoxListField"), exports);
+__exportStar(require("./BkModel/BkField/BkComboBoxField/BkComboBoxField"), exports);
+__exportStar(require("./BkModel/BkField/BkDateField/BkDateField"), exports);
+__exportStar(require("./BkModel/BkField/BkTimeField/BkTimeField"), exports);
+__exportStar(require("./BkModel/BkField/BkDateTimeField/BkDateTimeField"), exports);
+__exportStar(require("./BkModel/BkField/BkFileField/BkFileField"), exports);
+__exportStar(require("./BkModel/BkField/BkImageField/BkImageField"), exports);
+__exportStar(require("./BkModel/BkField/BkLabelField/BkLabelField"), exports);
+__exportStar(require("./BkModel/BkField/BkLinkField/BkLinkField"), exports);
+__exportStar(require("./BkModel/BkField/BkTextAreaField/BkTextAreaField"), exports);
+__exportStar(require("./BkModel/BkField/BkTextBoxField/BkTextBoxField"), exports);
+__exportStar(require("./BkModel/BkField/BkPhoneField/BkPhoneField"), exports);
+__exportStar(require("./BkModel/BkField/BkPasswordField/BkPasswordField"), exports);
+__exportStar(require("./BkModel/BkField/BkRadioField/BkRadioField"), exports);
+__exportStar(require("./BkModel/BkForm/BkForm"), exports);
+__exportStar(require("./BkModel/BkForm/BkRowForm/BkRowForm"), exports);
+__exportStar(require("./BkModel/BkForm/BkTableForm/BkTableForm"), exports);
+__exportStar(require("./BkModel/BkPage/BkPage"), exports);
+__exportStar(require("./BkModel/BkPageLink/BkPageLink"), exports);
+__exportStar(require("./BkModel/BkColumn/BkColumn"), exports);
+__exportStar(require("./BkModel/BkTable/BkTable"), exports);
+__exportStar(require("./BkModel/BkParam/BkParam"), exports);
+__exportStar(require("./BkModel/BkAction/BkAction"), exports);
