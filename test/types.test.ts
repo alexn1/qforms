a978bc39a1a32d4784c1707a1d6123692e615ea7
Alexander Nesterenko <alexn140@gmail.com>
2023-08-11 00:43:01 +0700

    test

--- test/types.test.ts
+++ test/types.test.ts
@@ -2,10 +2,10 @@ import { keyTupleToKey, keyToKeyTuple, Key } from '../src/types';
 
 describe('types', () => {
     test('keyTupleToKey', () => {
-        expect(keyTupleToKey([1, 2])).toBe('[1,2]');
+        expect(keyTupleToKey([1, 'abc', true])).toBe('[1,"abc",true]');
     });
 
     test('keyToKeyTuple', () => {
-        expect(keyToKeyTuple('[1]' as Key)).toEqual([1]);
+        expect(keyToKeyTuple('[1,"abc",true]' as Key)).toEqual([1, 'abc', true]);
     });
 });
