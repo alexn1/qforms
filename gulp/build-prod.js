65db75009afd5b90b715b9d811ab9a4cac2ca852
Alexander Nesterenko <alexn140@gmail.com>
2023-08-15 22:38:31 +0700

    clean on build prod

--- gulp/build-prod.js
+++ gulp/build-prod.js
@@ -1,12 +1,13 @@
 const Lib = require('./Lib');
 
 module.exports = async () => {
+    await Lib.exec('npx gulp clean');
     await Lib.exec('npx gulp copy-ejs');
     await Lib.exec('npx gulp copy-lib');
     await Lib.exec('npx gulp copy-img');
     // await Lib.exec('tsc --build tsconfig.back.json');
-    await Lib.exec('webpack --config webpack.config.back.index.js');
     await Lib.exec('webpack --config webpack.config.back.start.js');
+    await Lib.exec('webpack --config webpack.config.back.index.js');
     await Lib.exec('webpack --config webpack.config.index.js');
     await Lib.exec('webpack --config webpack.config.monitor.js');
     await Lib.exec('webpack --config webpack.config.editor.js');
