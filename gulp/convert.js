54a0ebb4d5b995ed54e9c812e0b4f2013d3a2ba5
Alexander Nesterenko <alexn140@gmail.com>
2023-09-01 15:03:18 +0700

    gulp task convert

--- gulp/convert.js
+++ gulp/convert.js
@@ -1,10 +1,8 @@
 async function convert() {
     console.debug('convert');
     const { Converter } = require('../../qforms');
-    // await Converter.reformat('./apps/demo/Application1.json');
-    // await Converter.reformat('./apps/demo2/Application1.json');
     await Converter.reformat('./apps/mongo/mongo.json');
-    // await Converter.reformat('./apps/test/test.json');
+    await Converter.reformat('./apps/test/test.json');
 }
 
 module.exports = convert;
