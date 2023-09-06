839b90061f148e70e95528b48d483e2829dec927
Alexander Nesterenko <alexn140@gmail.com>
2023-09-06 13:38:00 +0700

    copy lib by plugin

--- gulp/build-dev.js
+++ gulp/build-dev.js
@@ -3,7 +3,7 @@ const Lib = require('./Lib');
 module.exports = async () => {
     await Lib.exec('npx gulp clean');
     // await Lib.exec('npx gulp copy-ejs');
-    await Lib.exec('npx gulp copy-lib');
+    // await Lib.exec('npx gulp copy-lib');
     await Lib.exec('npx gulp copy-img');
     // await Lib.exec('tsc --build tsconfig.back.json');
     await Lib.exec('NODE_ENV=dev webpack --config webpack.config.back.start.js');
