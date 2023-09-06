4872786806d4ddb440fe480fedd134b21d3d6c13
Alexander Nesterenko <alexn140@gmail.com>
2023-09-06 13:54:57 +0700

    gulp ref

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,10 @@
-// release
-module.exports['release'] = require('./gulp/release');
-
-// deploy
-module.exports['deploy'] = require('./gulp/deploy');
-
-// copy
-module.exports['copy-ejs'] = require('./gulp/copy-ejs');
-module.exports['copy-lib'] = require('./gulp/copy-lib');
-module.exports['copy-img'] = require('./gulp/copy-img');
-
-// clean
-module.exports['clean'] = require('./gulp/clean');
-
 // build
 module.exports['build-dev'] = require('./gulp/build-dev');
 module.exports['build-prod'] = require('./gulp/build-prod');
+
+// release/deploy
+module.exports['release'] = require('./gulp/release');
+module.exports['deploy'] = require('./gulp/deploy');
 
 // docker
 module.exports['docker-build'] = require('./gulp/docker-build');
@@ -22,3 +12,11 @@ module.exports['docker-run'] = require('./gulp/docker-run');
 
 // convert
 module.exports['convert'] = require('./gulp/convert');
+
+// clean
+module.exports['clean'] = require('./gulp/clean');
+
+// copy
+module.exports['copy-ejs'] = require('./gulp/copy-ejs');
+module.exports['copy-lib'] = require('./gulp/copy-lib');
+module.exports['copy-img'] = require('./gulp/copy-img');
