e630737c8634f09fc31f5c2a93782b438c8afbbd
Alexander Nesterenko <alexn140@gmail.com>
2023-09-10 19:01:01 +0700

    chore: misc

--- gulp/build-prod.js
+++ gulp/build-prod.js
@@ -2,8 +2,8 @@ const Lib = require('./Lib');
 
 module.exports = async () => {
     await Lib.exec('npx gulp clean');
-    await Lib.exec('webpack --config webpack.config.back.start.js');
     await Lib.exec('webpack --config webpack.config.back.index.js');
+    await Lib.exec('webpack --config webpack.config.back.start.js');
     await Lib.exec('webpack --config webpack.config.editor.js');
     await Lib.exec('webpack --config webpack.config.index.js');
     await Lib.exec('webpack --config webpack.config.monitor.js');
