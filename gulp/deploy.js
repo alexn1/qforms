5991086f1b57d76e0c72bc042783d2c8ce9f575c
Alexander Nesterenko <alexn140@gmail.com>
2023-09-29 00:19:48 +0700

    npm publish not quiet

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -2,6 +2,6 @@ const Lib = require('./Lib');
 
 module.exports = async () => {
     await Lib.exec('git checkout release');
-    await Lib.exec('npm publish');
+    await Lib.exec('npm publish', false);
     await Lib.exec('git checkout master');
 };
