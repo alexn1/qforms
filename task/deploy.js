a07110071d21105a11e6a4d8280c6b0b30bb6aab
Alexander Nesterenko <alexn140@gmail.com>
2023-11-16 18:25:28 +0700

    fix: pull release branch on deploy

--- task/deploy.js
+++ task/deploy.js
@@ -4,6 +4,7 @@ main();
 
 async function main() {
     await Lib.exec('git checkout release');
+    await Lib.exec('git pull');
     await Lib.exec('npm publish', false);
     await Lib.exec('git checkout master');
 }
