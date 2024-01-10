9ad2d2e58e2a8113234089e73535ba34b48a5942
Alexander Nesterenko <alexn140@gmail.com>
2024-01-10 22:23:01 +0700

    parallel build:prod

--- task/build-prod.js
+++ task/build-prod.js
@@ -4,10 +4,27 @@ main();
 
 async function main() {
     await Lib.exec('npm run clean');
-    await Lib.exec('webpack --config webpack.config.back.index.js');
-    await Lib.exec('webpack --config webpack.config.back.start.js');
-    await Lib.exec('webpack --config webpack.config.editor.js');
+    await Promise.all([back(), front()]);
+}
+
+async function back() {
+    /* await Lib.exec('webpack --config webpack.config.back.index.js');
+    await Lib.exec('webpack --config webpack.config.back.start.js'); */
+    await Promise.all([
+        Lib.exec('webpack --config webpack.config.back.index.js'),
+        Lib.exec('webpack --config webpack.config.back.start.js'),
+    ]);
+}
+
+async function front() {
+    /* await Lib.exec('webpack --config webpack.config.editor.js');
     await Lib.exec('webpack --config webpack.config.index.js');
     await Lib.exec('webpack --config webpack.config.monitor.js');
-    await Lib.exec('webpack --config webpack.config.viewer.js');
+    await Lib.exec('webpack --config webpack.config.viewer.js'); */
+    await Promise.all([
+        Lib.exec('webpack --config webpack.config.editor.js'),
+        Lib.exec('webpack --config webpack.config.index.js'),
+        Lib.exec('webpack --config webpack.config.monitor.js'),
+        Lib.exec('webpack --config webpack.config.viewer.js'),
+    ]);
 }
