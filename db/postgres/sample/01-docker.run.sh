eab3d1ffbda97ef2d4683683e1afbd36accfef59
Alexander Nesterenko <alexn140@gmail.com>
2024-07-14 16:24:08 +0700

    fix: do not restart if stopped

--- db/postgres/sample/01-docker.run.sh
+++ db/postgres/sample/01-docker.run.sh
@@ -1,6 +1,6 @@
 # docker rm qforms-postgres-sample
 docker run --name qforms-postgres-sample \
---restart always \
+--restart unless-stopped \
 -p 5434:5432 \
 -e POSTGRES_PASSWORD=example \
 -v ~/data/qforms/postgres-sample:/var/lib/postgresql/data \
